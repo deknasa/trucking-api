bbbbcba271c6cc9012a6ab96e9f138d0e7802802
IT TAS <90301689+transporindo@users.noreply.github.com>
2022-01-14 17:12:50 +0700

    ryan14012022

---
+++ database/migrations/2022_01_14_100234_create_userrole_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserroleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userrole', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->default('0');
+            $table->unsignedBigInteger('role_id')->default('0');
+            $table->string('modifiedby',50)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('userrole');
+    }
+}
