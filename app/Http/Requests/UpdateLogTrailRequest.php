9e753eeaa45a21a9580e5783f43598be660f77bb
IT TAS <90301689+transporindo@users.noreply.github.com>
2022-01-17 17:26:59 +0700

    update17012022

---
+++ app/Http/Requests/UpdateLogTrailRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatelogtrailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'ntabel' => 'required',
+            'postfrom' => 'required',
+            'idtrans' => 'required',
+            'nobuktitrans' => 'required',
+            'aksi' => 'required',
+            'datajson' => 'required',
+            'modifiedby' => 'required',
+            
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'ntabel' => 'ntabel',
+            'postfrom' => 'postfrom',
+            'idtrans' => 'idtrans',
+            'nobuktitrans' => 'nobuktitrans',
+            'aksi' => 'aksi',
+            'datajson' => 'datajson',
+            'modifiedby' => 'modifiedby',
+        ];
+    }
+}
