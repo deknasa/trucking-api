5ace743bc48f2e7eb3796b8dce27b5c92a89284f
IT TAS <90301689+transporindo@users.noreply.github.com>
2022-02-11 17:36:37 +0700

    ryan11022022

---
+++ database/migrations/2022_02_11_172958_create_tarif_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarifTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarif', function (Blueprint $table) {
+            $table->id();
+            $table->string('tujuan',200)->default('');
+            $table->unsignedBigInteger('container_id')->default('0');
+            $table->double('nominal',15,2)->default('0');
+            $table->integer('statusaktif')->length(11)->default('0');
+            $table->string('tujuanasal',300)->default('');
+            $table->integer('sistemton')->length(11)->default('0');
+            $table->unsignedBigInteger('kota_id')->default('0');
+            $table->unsignedBigInteger('zona_id')->default('0');
+            $table->double('ton',15,2)->default('0');
+            $table->integer('batasberlaku')->length(11)->default('0');
+            $table->date('tglmulaiberlaku')->default('1900/1/1');
+            $table->date('tglakhirberlaku')->default('1900/1/1');
+            $table->integer('penyesuaianharga')->length(11)->default('0');
+            $table->string('modifiedby',50)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarif');
+    }
+}
