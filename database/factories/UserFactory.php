98fbb031be74ee04db722c705aed0e162943d21f
Hermes <hermestaspusat@gmail.com>
2022-09-14 15:05:28 +0700

    created api user unit test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Cabang;
+use App\Models\Parameter;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -19,6 +21,10 @@ class UserFactory extends Factory
             'user' => $this->faker->firstName(),
             'name' => $this->faker->firstName(),
             'password' => bcrypt('123456'),
+            'dashboard' => $this->faker->word(),
+            'cabang_id' => $this->faker->randomElement(Cabang::all()),
+            'karyawan_id' => $this->faker->randomElement(Parameter::where('grp', 'STATUS KARYAWAN')->get()),
+            'statusaktif' => $this->faker->randomElement(Parameter::where('grp', 'STATUS AKTIF')->get())
         ];
     }
 
