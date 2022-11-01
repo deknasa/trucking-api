617b4a1e4d64255f02697c6ba43baee2ecc24e46
IT TAS <90301689+transporindo@users.noreply.github.com>
2022-11-01 17:14:58 +0700

    ryan01112022

---
+++ app/Http/Controllers/Api/HariLiburController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HariLibur;
+use App\Http\Requests\StoreHariLiburRequest;
+use App\Http\Requests\UpdateHariLiburRequest;
+
+class HariLiburController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreHariLiburRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreHariLiburRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HariLibur  $hariLibur
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HariLibur $hariLibur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HariLibur  $hariLibur
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HariLibur $hariLibur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateHariLiburRequest  $request
+     * @param  \App\Models\HariLibur  $hariLibur
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateHariLiburRequest $request, HariLibur $hariLibur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HariLibur  $hariLibur
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HariLibur $hariLibur)
+    {
+        //
+    }
+}
---
+++ app/Models/HariLibur.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HariLibur extends MyModel
+{
+    use HasFactory;
+
+
+    protected $table = 'error';
+
+    protected $casts = [
+        'created_at' => 'date:d-m-Y H:i:s',
+        'updated_at' => 'date:d-m-Y H:i:s'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+}
