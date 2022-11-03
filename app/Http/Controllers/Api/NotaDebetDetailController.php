3ad62455b737834279367c8dae17e1e3955c6a8e
iqbalrafli13 <iqbal13rafli@gmail.com>
2022-11-03 17:09:52 +0700

    Nota debet and nota kredit report export

--- app/Http/Controllers/Api/NotaDebetDetailController.php
+++ app/Http/Controllers/Api/NotaDebetDetailController.php
@@ -1,6 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 use App\Models\NotaDebetDetail;
 use App\Http\Requests\StoreNotaDebetDetailRequest;
@@ -13,74 +19,124 @@ class NotaDebetDetailController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $params = [
+            'id' => $request->id,
+            'notadebet_id' => $request->notadebet_id,
+            'withHeader' => $request->withHeader ?? false,
+            'whereIn' => $request->whereIn ?? [],
+            'forReport' => $request->forReport ?? false,
+            'sortIndex' => $request->sortOrder ?? 'id',
+            'sortOrder' => $request->sortOrder ?? 'asc',
+        ];
+        try {
+            $query = NotaDebetDetail::from('notadebetdetail as detail');
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+            if (isset($params['id'])) {
+                $query->where('detail.id', $params['id']);
+            }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreNotaDebetDetailRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+            if (isset($params['notadebet_id'])) {
+                $query->where('detail.notadebet_id', $params['notadebet_id']);
+            }
+
+            if (count($params['whereIn']) > 0) {
+                $query->whereIn('notadebet_id', $params['whereIn']);
+            }
+
+            if ($params['forReport']) {
+                $query->select(
+                    "detail.id",
+                    "detail.notadebet_id",
+                    "detail.nobukti",
+                    "detail.tglterima",
+                    "detail.invoice_nobukti",
+                    "detail.nominal",
+                    "detail.nominalbayar",
+                    "detail.lebihbayar",
+                    "detail.keterangan",
+                    "detail.coalebihbayar",
+                    "detail.modifiedby"
+                );
+
+                $notadebet = $query->get();
+            } else {
+                $query->select(
+                    "detail.id",
+                    "detail.notadebet_id",
+                    "detail.nobukti",
+                    "detail.tglterima",
+                    "detail.invoice_nobukti",
+                    "detail.nominal",
+                    "detail.nominalbayar",
+                    "detail.lebihbayar",
+                    "detail.keterangan",
+                    "detail.coalebihbayar",
+                    "detail.modifiedby"
+                )
+                // ->leftJoin('pengeluaranstok','pengeluaranstokheader.pengeluaranstok_id','pengeluaranstok.id')
+
+                ->leftJoin('notadebetheader', 'detail.notadebet_id', 'notadebetheader.id')
+                ->leftJoin('invoiceheader', 'detail.invoice_nobukti', 'invoiceheader.nobukti')
+                ->leftJoin('akunpusat', 'detail.coalebihbayar', 'akunpusat.coa');
+                $notadebet = $query->get();
+            }
+
+            return response([
+                'data' => $notadebet
+            ]);
+        } catch (\Throwable $th) {
+            return response([
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
     public function store(StoreNotaDebetDetailRequest $request)
     {
-        //
-    }
+        DB::beginTransaction();
+        $validator = Validator::make($request->all(), [
+            'keterangandetail' => 'required',
+         ], [
+             'keterangandetail.required' => ':attribute' . ' ' . app(ErrorController::class)->geterror('WI')->keterangan,
+              ], [
+             'keterangandetail' => 'keterangan Detail',
+            ],
+         );         
+         if (!$validator->passes()) {
+            return [
+                'error' => true,
+                'errors' => $validator->messages()
+            ];
+        }
+        try {
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\NotaDebetDetail  $notaDebetDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function show(NotaDebetDetail $notaDebetDetail)
-    {
-        //
-    }
+            $notaDebetDetail = new NotaDebetDetail();
+            $notaDebetDetail->notadebet_id = $request->notadebet_id;
+            $notaDebetDetail->nobukti = $request->nobukti;
+            $notaDebetDetail->tglterima = $request->tglterima;
+            $notaDebetDetail->invoice_nobukti = $request->invoice_nobukti;
+            $notaDebetDetail->nominal = $request->nominal;
+            $notaDebetDetail->nominalbayar = $request->nominalbayar;
+            $notaDebetDetail->lebihbayar = $request->lebihbayar;
+            $notaDebetDetail->keterangan = $request->keterangandetail;
+            $notaDebetDetail->coalebihbayar = $request->coalebihbayar;
+            $notaDebetDetail->modifiedby = $request->modifiedby;
+            
+            DB::commit();
+            if ($notaDebetDetail->save()) {
+                return [
+                    'error' => false,
+                    'id' => $notaDebetDetail->id,
+                    'data' => $notaDebetDetail,
+                    'tabel' => $notaDebetDetail->getTable(),
+                ];
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+            DB::rollBack();
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\NotaDebetDetail  $notaDebetDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(NotaDebetDetail $notaDebetDetail)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateNotaDebetDetailRequest  $request
-     * @param  \App\Models\NotaDebetDetail  $notaDebetDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateNotaDebetDetailRequest $request, NotaDebetDetail $notaDebetDetail)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\NotaDebetDetail  $notaDebetDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(NotaDebetDetail $notaDebetDetail)
-    {
-        //
+        
     }
 }
