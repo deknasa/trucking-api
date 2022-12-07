2123edfbb6f092c929caf9743d1faaf5ae7bb37d
IT TAS <90301689+transporindo@users.noreply.github.com>
2022-12-07 10:18:01 +0700

    ryan071220221017

--- database/migrations/2022_09_27_000003_create_hutangheader_table.php
+++ database/migrations/2022_09_27_000003_create_hutangheader_table.php
@@ -29,6 +29,7 @@ class CreateHutangheaderTable extends Migration
             $table->integer('statuscetak')->Length(11)->default('0');
             $table->string('userbukacetak',50)->default('');
             $table->date('tglbukacetak')->default('1900/1/1');
+            $table->integer('jumlahcetak')->Length(11)->default('0');
             $table->string('modifiedby', 50)->default('');            
             $table->timestamps();
 
