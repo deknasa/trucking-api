c1b317051e49fa5d42535aa409cde578473c97b3
bshf <bungashafa53@gmail.com>
2022-12-26 15:45:42 +0700

    update kartustok controller

--- app/Http/Controllers/Api/KartuStokController.php
+++ app/Http/Controllers/Api/KartuStokController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Gandengan;
+use App\Models\Gudang;
 use App\Models\KartuStok;
+use App\Models\Parameter;
+use App\Models\Stok;
+use App\Models\Trado;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -28,16 +33,34 @@ class KartuStokController extends Controller
     
     public function report(Request $request)
     {
-        $stokdari_id = $request->stokdari_id;
-        $stoksampai_id = $request->stoksampai_id;
-        $dari = $request->dari;
-        $sampai = $request->sampai;
-        $filter = $request->filter;
-        $datafilter = $request->datafilter;
-        
-        $report = KartuStok::getReport($stokdari_id, $stoksampai_id, $dari,$sampai, $filter, $datafilter);
+        $kartuStok = new KartuStok();
+
+        $stokdari_id = Stok::find($request->stokdari_id);
+        $stoksampai_id = Stok::find($request->stoksampai_id);
+        $filter = Parameter::find($request->filter);
+        if($filter->text == 'GUDANG'){
+            $getdatafilter = Gudang::find($request->datafilter);
+            $datafilter =$getdatafilter->gudang;
+        } else if($filter->text == 'TRADO'){
+            $getdatafilter = Trado::find($request->datafilter);
+            $datafilter =$getdatafilter->keterangan;
+        } else if($filter->text == 'GANDENGAN'){
+            $getdatafilter = Gandengan::find($request->datafilter);
+            $datafilter =$getdatafilter->keterangan;
+        } 
+
+        $report = [
+            'stokdari' => $stokdari_id->namastok,
+            'stoksampai' => $stoksampai_id->namastok,
+            'dari' => $request->dari,
+            'sampai' => $request->sampai,
+            'filter' => $filter->text,
+            'datafilter' => $datafilter
+        ];
+
         return response([
-            'data' => $report
+            'data' => $kartuStok->get(),
+            'dataheader' => $report
         ]);
     }
 }
