54f7ecc9f7a32cef83d92dc886533231b202763f
bshf <bungashafa53@gmail.com>
2022-12-26 11:23:55 +0700

    create laporan kasbank 26/12

---
+++ app/Models/LaporanKasBank.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class LaporanKasBank extends MyModel
+{
+    use HasFactory;
+
+    protected $table = 'pengeluaranstokdetailfifo';
+
+    protected $casts = [
+        'created_at' => 'date:d-m-Y H:i:s',
+        'updated_at' => 'date:d-m-Y H:i:s'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+
+
+    public function getReport($dari, $sampai, $bank_id)
+    {
+        // data coba coba
+        $query = DB::table('pengeluaranheader')->select(
+            'pengeluaranheader.id',
+            'pengeluaranheader.nobukti',
+            'pengeluaranheader.keterangan',
+            'bank.namabank',
+            'cabang.namacabang',
+        )
+        ->leftJoin('bank', 'pengeluaranheader.bank_id', 'bank.id')
+        ->leftJoin('cabang', 'pengeluaranheader.cabang_id', 'cabang.id')
+        ->where('pengeluaranheader.bank_id', $bank_id);
+
+        $data = $query->get();
+        return $data;
+    }
+}
