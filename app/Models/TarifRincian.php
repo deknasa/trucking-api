f0f0e09dc549c5fe5002820ecfadde496cb076de
IT TAS <90301689+transporindo@users.noreply.github.com>
2023-01-18 17:17:58 +0700

    ryan180120230517

---
+++ app/Models/TarifRincian.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class TarifRincian extends MyModel
+{
+    use HasFactory;
+
+    protected $table = 'tarifrincian';
+
+    protected $casts = [
+        'created_at' => 'date:d-m-Y H:i:s',
+        'updated_at' => 'date:d-m-Y H:i:s'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function getAll($id)
+    {
+        $query = DB::table('tarifrincian')->from(DB::raw("tarifrincian with (readuncommitted)"))
+        ->select(
+            'tarifrincian.container_id',
+            'container.keterangan as container',
+            'tarifrincian.nominal',
+        )
+            ->leftJoin('container', 'container.id', 'tarifrincian.container_id')
+            ->where('upahsupir_id', '=', $id);
+
+
+        $data = $query->get();
+
+        return $data;
+    }
+
+    public function setUpRow()
+    {
+        $query = DB::table('container')->select(
+            'container.keterangan as container',
+            'container.id as container_id'
+        );
+
+        return $query->get();
+    }
+    public function setUpRowExcept($rincian)
+    {
+        $data = DB::table('container')->select(
+            'container.keterangan as container',
+            'container.id as container_id'
+        );
+        $temp = '##tempcrossjoin' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
+
+        Schema::create($temp, function ($table) {
+            $table->increments('id');
+            $table->string('container')->default('');
+            $table->string('containerId')->default('0');
+        });
+
+        DB::table($temp)->insertUsing([
+            "container",
+            "containerId"
+        ], $data);
+
+        //select yang sudah ada
+        $except = DB::table($temp)->select(
+            "$temp.id",
+        );
+        for ($i=0; $i < count($rincian); $i++) { 
+            $except->orWhere(function ($query) use ($rincian, $i) {
+                $query->where('containerId', $rincian[$i]['container_id']);
+            });
+        }
+        
+        foreach ($except->get() as $e) {
+            $arr[] = $e->id;
+        }
+        
+        //select semua keluali
+        $query = DB::table($temp)->select(
+            "$temp.id",
+            "$temp.container",
+            "$temp.containerId as container_id"
+        )->whereNotIn('id',$arr);
+
+        // ->whereRaw(" NOT EXIST  ( select $temp.statuscontainer, $temp.container from   [$temp]  WHERE (statuscontainer = 'empty' and container = '20`') or (statuscontainer = 'FULL' and container = '40`') ) ");
+        // ->whereRaw("(statuscontainer = 'FULL' and container = '40`')");
+
+        return $query->get();
+    }
+}
