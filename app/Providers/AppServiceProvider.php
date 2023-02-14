0cef0c8e0438d93e5d8c0d971dd221f48917f455
Hermes Agistrian <hermes8agistrian03@gmail.com>
2023-02-14 14:49:01 +0700

    update kas gantung index

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Container\Container;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // 
+        Builder::macro('sort', function(callable $handler) {
+            return $handler();
+        });
     }
 }
