38d2fd76b9475038167f481488198cec5b3df415
RAFARDHAN ATHALLA <pt.transporindo@gmail.com>
2023-03-13 09:42:57 +0700

    ryan130320230942

--- database/seeders/BankSeeder.php
+++ database/seeders/BankSeeder.php
@@ -20,5 +20,7 @@ class BankSeeder extends Seeder
 
         Bank::create([  'kodebank' => 'KAS TRUCKING',  'namabank' => 'KAS TRUCKING',  'coa' => '01.01.01.02',  'tipe' => 'KAS',  'formatpenerimaan' => '32',  'formatpengeluaran' => '33',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
         Bank::create([  'kodebank' => 'BANK TRUCKING',  'namabank' => 'BANK TRUCKING',  'coa' => '01.02.02.01',  'tipe' => 'BANK',  'formatpenerimaan' => '87',  'formatpengeluaran' => '88',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
+        Bank::create([  'kodebank' => 'BANK TRUCKING2',  'namabank' => 'BCA 8195147911',  'coa' => '01.02.02.03',  'tipe' => 'BANK',  'formatpenerimaan' => '266',  'formatpengeluaran' => '267',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
+        Bank::create([  'kodebank' => 'BANK TRUCKING3',  'namabank' => 'BCA 8195157088',  'coa' => '01.02.02.05',  'tipe' => 'BANK',  'formatpenerimaan' => '268',  'formatpengeluaran' => '269',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
     }
 }
