2f13f87b9e89d1c2591c0321140f7f0b8de1ac99
RAFARDHAN ATHALLA <pt.transporindo@gmail.com>
2023-03-13 14:07:23 +0700

    ryan130320230201

--- database/seeders/ZonaSeeder.php
+++ database/seeders/ZonaSeeder.php
@@ -18,6 +18,13 @@ class ZonaSeeder extends Seeder
         DB::statement("delete zona");
         DB::statement("DBCC CHECKIDENT ('zona', RESEED, 1);");
 
-        zona::create(['zona' => 'luar kota',  'keterangan' => 'luar kota',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',]);
+
+        Zona::create(['zona' => 'ZONA I', 'keterangan' => 'ZONA I', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
+        Zona::create(['zona' => 'ZONA II', 'keterangan' => 'ZONA II', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
+        Zona::create(['zona' => 'ZONA III', 'keterangan' => 'ZONA III', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
+        Zona::create(['zona' => 'ZONA IV', 'keterangan' => 'ZONA IV', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
+        Zona::create(['zona' => 'ZONA V', 'keterangan' => 'ZONA V', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
+        Zona::create(['zona' => 'ZONA VI', 'keterangan' => 'ZONA VI', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
+        Zona::create(['zona' => 'ZONA VII', 'keterangan' => 'ZONA VII', 'modifiedby' => 'RYAN', 'statusaktif' => '1',]);
     }
 }
