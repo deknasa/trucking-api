8587f43986d4b5647e4ad0eedb5c76c311ee09b7
RAFARDHAN ATHALLA <pt.transporindo@gmail.com>
2023-04-06 09:10:50 +0700

    ryan060420230910

--- database/seeders/BankSeeder.php
+++ database/seeders/BankSeeder.php
@@ -22,6 +22,6 @@ class BankSeeder extends Seeder
         Bank::create([  'kodebank' => 'BANK TRUCKING',  'namabank' => 'BANK TRUCKING',  'coa' => '01.02.02.01',  'tipe' => 'BANK',  'formatpenerimaan' => '87',  'formatpengeluaran' => '88',  'statusdefault' => '59',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
         Bank::create([  'kodebank' => 'BANK TRUCKING2',  'namabank' => 'BCA 8195147911',  'coa' => '01.02.02.03',  'tipe' => 'BANK',  'formatpenerimaan' => '266',  'formatpengeluaran' => '267',  'statusdefault' => '59',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
         Bank::create([  'kodebank' => 'BANK TRUCKING3',  'namabank' => 'BCA 8195157088',  'coa' => '01.02.02.05',  'tipe' => 'BANK',  'formatpenerimaan' => '268',  'formatpengeluaran' => '269',  'statusdefault' => '59',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
-        Bank::create([  'kodebank' => 'PENGEMBALIAN KE PUSAT',  'namabank' => 'PENGEMBALIAN KE PUSAT BCA 8195157088',  'coa' => '01.02.02.05',  'tipe' => 'BANK',  'formatpenerimaan' => '',  'formatpengeluaran' => '277',  'statusdefault' => '59',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
+        Bank::create([  'kodebank' => 'PENGEMBALIAN KE PUSAT',  'namabank' => 'PENGEMBALIAN KE PUSAT BCA 8195157088',  'coa' => '01.02.02.05',  'tipe' => 'BANK',  'formatpenerimaan' => '277',  'formatpengeluaran' => '277',  'statusdefault' => '59',  'statusaktif' => '1',  'modifiedby' => 'ADMIN',  ]);
     }
 }
