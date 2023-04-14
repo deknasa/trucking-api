5e8a120eaef90e1d9faa491543fd33cdeb09d351
RAFARDHAN ATHALLA <pt.transporindo@gmail.com>
2023-04-14 14:33:05 +0700

    ryan140420230233

--- database/seeders/AlatBayarSeeder.php
+++ database/seeders/AlatBayarSeeder.php
@@ -19,8 +19,14 @@ class AlatBayarSeeder extends Seeder
         DB::statement("DBCC CHECKIDENT ('AlatBayar', RESEED, 1);");
 
         AlatBAyar::create(['kodealatbayar' => 'TUNAI', 'namaalatbayar' => 'tunai', 'keterangan' => 'tunai', 'statuslangsungcair' => '56', 'statusdefault' => '58', 'statusaktif' => '1', 'bank_id' => '1', 'modifiedby' => 'ADMIN', 'coa' => '01.01.01.02',]);
-        AlatBAyar::create(['kodealatbayar' => 'TRANSFER', 'namaalatbayar' => 'TRANSFER', 'keterangan' => 'TRANSFER', 'statuslangsungcair' => '56', 'statusdefault' => '59', 'statusaktif' => '1', 'bank_id' => '2', 'modifiedby' => 'ADMIN', 'coa' => '01.02.02.01',]);
+        AlatBAyar::create(['kodealatbayar' => 'TRANSFER', 'namaalatbayar' => 'TRANSFER', 'keterangan' => 'TRANSFER', 'statuslangsungcair' => '56', 'statusdefault' => '58', 'statusaktif' => '1', 'bank_id' => '2', 'modifiedby' => 'ADMIN', 'coa' => '01.02.02.01',]);
         AlatBAyar::create(['kodealatbayar' => 'GIRO', 'namaalatbayar' => 'GIRO', 'keterangan' => '1', 'statuslangsungcair' => '57', 'statusdefault' => '59', 'statusaktif' => '1', 'bank_id' => '2', 'modifiedby' => 'ADMIN', 'coa' => '03.02.02.05',]);
+        AlatBAyar::create(['kodealatbayar' => 'TRANSFER', 'namaalatbayar' => 'TRANSFER', 'keterangan' => 'TRANSFER', 'statuslangsungcair' => '56', 'statusdefault' => '58', 'statusaktif' => '1', 'bank_id' => '3', 'modifiedby' => 'ADMIN', 'coa' => '01.02.02.01',]);
+        AlatBAyar::create(['kodealatbayar' => 'GIRO', 'namaalatbayar' => 'GIRO', 'keterangan' => '1', 'statuslangsungcair' => '57', 'statusdefault' => '59', 'statusaktif' => '1', 'bank_id' => '3', 'modifiedby' => 'ADMIN', 'coa' => '03.02.02.05',]);
+        AlatBAyar::create(['kodealatbayar' => 'TRANSFER', 'namaalatbayar' => 'TRANSFER', 'keterangan' => 'TRANSFER', 'statuslangsungcair' => '56', 'statusdefault' => '58', 'statusaktif' => '1', 'bank_id' => '4', 'modifiedby' => 'ADMIN', 'coa' => '01.02.02.01',]);
+        AlatBAyar::create(['kodealatbayar' => 'GIRO', 'namaalatbayar' => 'GIRO', 'keterangan' => '1', 'statuslangsungcair' => '57', 'statusdefault' => '59', 'statusaktif' => '1', 'bank_id' => '4', 'modifiedby' => 'ADMIN', 'coa' => '03.02.02.05',]);
+        AlatBAyar::create(['kodealatbayar' => 'TRANSFER', 'namaalatbayar' => 'TRANSFER', 'keterangan' => 'TRANSFER', 'statuslangsungcair' => '56', 'statusdefault' => '58', 'statusaktif' => '1', 'bank_id' => '5', 'modifiedby' => 'ADMIN', 'coa' => '01.02.02.01',]);
+        AlatBAyar::create(['kodealatbayar' => 'GIRO', 'namaalatbayar' => 'GIRO', 'keterangan' => '1', 'statuslangsungcair' => '57', 'statusdefault' => '59', 'statusaktif' => '1', 'bank_id' => '5', 'modifiedby' => 'ADMIN', 'coa' => '03.02.02.05',]);
        
 
 
