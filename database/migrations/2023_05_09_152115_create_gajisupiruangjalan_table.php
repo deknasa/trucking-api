6b0051b32a2df611ce140bd777b5c5a36d0c6946
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-05-09 15:27:50 +0700

    add table gajisupiruangjalan

---
+++ database/migrations/2023_05_09_152115_create_gajisupiruangjalan_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateGajisupiruangjalanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gajisupiruangjalan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('gajisupir_id')->nullable();   
+            $table->string('gajisupir_nobukti', 50)->nullable();            
+            $table->string('absensisupir_nobukti', 50)->nullable();            
+            $table->unsignedBigInteger('supir_id')->nullable();   
+            $table->double('nominal', 15,2)->nullable();                     
+            $table->timestamps();
+
+            $table->foreign('supir_id', 'gajisupiruangjalan_supir_supir_id_foreign')->references('id')->on('supir');
+            $table->foreign('gajisupir_id', 'gajisupiruangjalan_gajisupirheader_gajisupir_id_foreign')->references('id')->on('gajisupirheader');    
+        });
+
+        DB::statement("ALTER TABLE gajisupiruangjalan NOCHECK CONSTRAINT gajisupiruangjalan_supir_supir_id_foreign");
+        DB::statement("ALTER TABLE gajisupiruangjalan NOCHECK CONSTRAINT gajisupiruangjalan_gajisupirheader_gajisupir_id_foreign");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gajisupiruangjalan');
+    }
+}
