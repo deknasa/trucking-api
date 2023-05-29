7b20378f396666fc45e9b94fd42fe3ba0de3ddb1
syarifa20 <syaripah2004@gmail.com>
2023-05-29 11:36:52 +0700

    destroy pendapatan

--- app/Http/Requests/DestroyPendapatanSupirRequest.php
+++ app/Http/Requests/DestroyPendapatanSupirRequest.php
@@ -36,57 +36,8 @@ class DestroyPendapatanSupirHeaderRequest extends FormRequest
                 "required",
                 new DateTutupBuku()
             ],
-            'bank' => 'required',
-            'tgldari' => [
-                'required', 'date_format:d-m-Y',
-                'before:'.$tglbatasakhir,
-                'after_or_equal:'.$tglbatasawal,
-            ],
-            'tglsampai' => [
-                'required', 'date_format:d-m-Y',
-                'before:'.$tglbatasakhir,
-                'after_or_equal:'.$this->tgldari 
-            ],
-            'periode' => [
-                'required', 'date_format:d-m-Y',
-                'before:'.$tglbatasakhir,
-            ],
+           
         ];
-        $relatedRequests = [
-            UpdatePendapatanSupirDetailRequest::class
-        ];
-
-        foreach ($relatedRequests as $relatedRequest) {
-            $rules = array_merge(
-                $rules,
-                (new $relatedRequest)->rules()
-            );
-        }
-
         return $rules;
-    }
-
-    public function attributes()
-    {
-        return [
-            'tglbukti' => 'tanggal bukti',
-            'tgldari' => 'tanggal dari',
-            'tglsampai' => 'tanggal sampai',
-            'supir.*' => 'supir',
-            'nominal.*' => 'nominal',
-            'keterangan_detail.*' => 'keterangan'
-        ];
-    }
-
-    public function messages()
-    {
-        $tglbatasakhir = (date('Y') + 1) . '-01-01';
-        return [
-            'nominal.*.gt' => 'tidak boleh kosong',
-            'tglbukti.date_format' => app(ErrorController::class)->geterror('DF')->keterangan,
-            'tgldari.before' => app(ErrorController::class)->geterror('NTLB')->keterangan. ' '.$tglbatasakhir,
-            'tglsampai.before' => app(ErrorController::class)->geterror('NTLB')->keterangan. ' '.$tglbatasakhir,
-            'periode.before' => app(ErrorController::class)->geterror('NTLB')->keterangan. ' '.$tglbatasakhir,
-        ];
     }
 }
