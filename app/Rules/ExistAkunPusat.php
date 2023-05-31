23c6312a013774acc52b2d7d2b8f38f58a476fe6
bshf <bungashafa53@gmail.com>
2023-05-31 08:30:02 +0700

    create rule for exist supir,agen,bank

---
+++ app/Rules/ExistAkunPusat.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Rules;
+
+use App\Http\Controllers\Api\ErrorController;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class ExistAkunPusat implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $akunPusat = DB::table("akunpusat")->from(DB::raw("akunpusat with (readuncommitted)"))
+            ->where('coa', $value)
+            ->first();
+        if($akunPusat == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return app(ErrorController::class)->geterror('TVD')->keterangan;
+    }
+}
---
+++ app/Rules/ExistAlatBayar.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Rules;
+
+use App\Http\Controllers\Api\ErrorController;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class ExistAlatBayar implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $alatbayar = DB::table("alatbayar")->from(DB::raw("alatbayar with (readuncommitted)"))
+            ->where('id', $value)
+            ->first();
+        if($alatbayar == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return app(ErrorController::class)->geterror('TVD')->keterangan;
+    }
+}
