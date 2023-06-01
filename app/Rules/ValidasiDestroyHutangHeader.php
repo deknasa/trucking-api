ca74ad64486de046cf566357a83a7a3b8f6db67c
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-06-01 09:53:17 +0700

    perbaikan rules

---
+++ app/Rules/ValidasiDestroyHutangHeader.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidasiDestroyHutangHeader implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($param, $paramcetak)
+    {
+        $this->kondisi = $param;
+        $this->kondisicetak = $paramcetak;
+    }
+    public $kondisi;
+    public $kondisicetak;
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if ($this->kondisi == true) {
+            return false;
+        } else if ($this->kondisicetak == true) {
+            return false;
+        } else {
+
+            return true;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return app(ErrorController::class)->geterror('SATL')->keterangan;
+    }
+}
