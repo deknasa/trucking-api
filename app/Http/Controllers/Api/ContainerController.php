08d4dc39e26380117cc2a1bf76c3482342fee547
rahmadqolbi <rahmadqolbi123@gmail.com>
2023-06-02 18:27:02 +0700

    export container

--- app/Http/Controllers/Api/ContainerController.php
+++ app/Http/Controllers/Api/ContainerController.php
@@ -302,7 +302,9 @@ class ContainerController extends Controller
 
 
             $containers[$i]['statusaktif'] = $statusaktif;
-
+            
+            $nominalsumbangan = number_format($params['nominalsumbangan'], 2, ',', '.');
+            $containers[$i]['nominalsumbangan'] = $nominalsumbangan;
         
             $i++;
 
