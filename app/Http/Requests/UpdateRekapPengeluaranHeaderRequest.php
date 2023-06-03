637ac26fe9e46032c3275add474cd05b9a59afcc
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-06-03 16:21:32 +0700

    perbaikan seeder

--- app/Http/Requests/UpdateRekapPengeluaranHeaderRequest.php
+++ app/Http/Requests/UpdateRekapPengeluaranHeaderRequest.php
@@ -3,12 +3,15 @@
 namespace App\Http\Requests;
 
 use App\Http\Controllers\Api\ErrorController;
+use App\Models\RekapPengeluaranheader;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Rules\DateTutupBuku;
 use App\Rules\ExistBank;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Validation\Rule;
+use App\Rules\ValidasiDestroyRekapPengeluaranHeader ;
+use App\Http\Controllers\Api\RekapPengeluaranHeaderController;
 
 class UpdateRekapPengeluaranHeaderRequest extends FormRequest
 {
@@ -30,6 +33,18 @@ class UpdateRekapPengeluaranHeaderRequest extends FormRequest
     public function rules()
     {
 
+        $controller = new RekapPengeluaranHeaderController;
+        $rekappengeluaranheader = new RekapPengeluaranHeader();
+        $cekdata = $rekappengeluaranheader->cekvalidasiaksi($this->nobukti);
+        $cekdatacetak = $controller->cekvalidasi($this->id);
+        if ($cekdatacetak->original['kodestatus']=='1') {
+                $cekdtcetak=true;
+        } else {
+            $cekdtcetak=false;
+        }
+        
+
+
         $query=DB::table('rekappengeluaranheader')->from(
             DB::raw('rekappengeluaranheader a with (readuncommitted)')
         )
@@ -45,6 +60,7 @@ class UpdateRekapPengeluaranHeaderRequest extends FormRequest
 
 
         $rules = [
+            'id' => [ new ValidasiDestroyRekapPengeluaranHeader($cekdata['kondisi'],$cekdtcetak)],
             'tglbukti' => [
                 'required', 'date_format:d-m-Y',
                 new DateTutupBuku(),
