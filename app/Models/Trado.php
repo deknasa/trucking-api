893a081fc36bc231c79dc3e4cafed13cb8b04e67
syarifa20 <syaripah2004@gmail.com>
2023-06-16 16:36:24 +0700

    add tgl cetak

--- app/Models/Trado.php
+++ app/Models/Trado.php
@@ -217,7 +217,7 @@ class Trado extends MyModel
                 'trado.updated_at',
                 DB::raw("'Laporan Trado' as judulLaporan"),
                 DB::raw("'" . $getJudul->text . "' as judul"),
-                DB::raw("'Tanggal Cetak : '+format(getdate(),'dd-MM-yyyy HH:mm:ss')+' User :".auth('api')->user()->name."' as tglcetak")
+                DB::raw("'Tanggal Cetak : '+format(getdate(),'dd-MM-yyyy HH:mm:ss')+' User :".auth('api')->user()->name."' as tglcetak") 
             )
             ->leftJoin(DB::raw("parameter as parameter_statusaktif with (readuncommitted)"), 'trado.statusaktif', 'parameter_statusaktif.id')
             ->leftJoin(DB::raw("parameter as parameter_statusjenisplat with (readuncommitted)"), 'trado.statusjenisplat', 'parameter_statusjenisplat.id')
