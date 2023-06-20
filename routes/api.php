79b7e63ea172d96da6909cc09afd153c8d6f26b7
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-06-20 11:26:51 +0700

    autorize disable

--- routes/api.php
+++ routes/api.php
@@ -241,7 +241,7 @@ Route::get('trado/image/{field}/{filename}/{type}/{aksi}', [TradoController::cla
 Route::get('stok/{filename}/{type}', [StokController::class, 'getImage']);
 Route::get('upahsupir/{filename}/{type}', [UpahSupirController::class, 'getImage']);
 
-route::middleware(['auth:api', 'authorized'])->group(function () {
+route::middleware(['auth:api'])->group(function () {
     
 
     Route::get('kota/combo', [KotaController::class, 'combo']);
