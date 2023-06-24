13d4f299e18c0f547b54906cecaa424d4e75c6f5
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-06-24 15:55:18 +0700

    perbaikan seeder

--- app/Models/PerkiraanLabaRugi.php
+++ app/Models/PerkiraanLabaRugi.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
-class PerkiraanLabaRugi extends Model
+class PerkiraanLabaRugi extends MyModel
 {
     use HasFactory;
+    
+    protected $table = 'perkiraanlabarugi';
+
+    protected $casts = [
+        'created_at' => 'date:d-m-Y H:i:s',
+        'updated_at' => 'date:d-m-Y H:i:s',
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
