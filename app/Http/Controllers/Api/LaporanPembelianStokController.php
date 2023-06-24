84355667b77c6231a319c608706c485011847dcf
rahmadqolbi <rahmadqolbi123@gmail.com>
2023-06-24 10:03:15 +0700

    laporan pembelian stok

---
+++ app/Http/Controllers/Api/LaporanPembelianStokController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GetIndexRangeRequest;
+use App\Http\Requests\ReportLaporanPembelianRequest;
+use App\Models\LaporanPembelianStok;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class LaporanPembelianStokController extends Controller
+{
+    /**
+     * @ClassName
+     */
+    public function index(Request $request)
+    {
+        return response([
+            'data' => [],
+            'attributes' => [
+                'totalRows' => 0,
+                'totalPages' => 0
+            ]
+        ]);
+    }
+
+    
+    /**
+     * @ClassName
+     */
+    public function report(Request $request)
+    {
+        $dari = date('Y-m-d', strtotime($request->dari));
+        $sampai = date('Y-m-d', strtotime($request->sampai));
+        $stokdari = $request->stokdari;
+        $stoksampai = $request->stoksampai;
+     
+
+        $laporanpembelianstok = new LaporanPembelianStok();
+
+       
+
+        $laporan_pembelianstok= $laporanpembelianstok->getReport($dari, $sampai,$stokdari, $stoksampai);
+        foreach($laporan_pembelianstok as $item){
+            $item->tglbukti = date('d-m-Y', strtotime($item->tglbukti));
+        }
+      
+        return response([
+            'data' => $laporan_pembelianstok
+            // 'data' => $report
+        ]);
+    }
+
+   /**
+     * @ClassName
+     */
+    public function export(Request $request)
+    {
+        $dari = date('Y-m-d', strtotime($request->dari));
+        $sampai = date('Y-m-d', strtotime($request->sampai));
+        $stokdari = $request->stokdari;
+        $stoksampai = $request->stoksampai;
+
+        $laporanpembelianstok = new LaporanPembelianStok();
+
+       
+
+        $laporan_pembelianstok= $laporanpembelianstok->getExport($dari, $sampai,$stokdari, $stoksampai);
+        foreach($laporan_pembelianstok as $item){
+            $item->tglbukti = date('d-m-Y', strtotime($item->tglbukti));
+        }
+      
+        return response([
+            'data' => $laporan_pembelianstok
+            // 'data' => $report
+        ]);
+    }
+    
+}
---
+++ app/Models/LaporanPembelianStok.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class LaporanPembelianStok extends MyModel
+{
+    use HasFactory;
+
+    protected $table = 'laporanpembelian';
+
+    protected $casts = [
+        'created_at' => 'date:d-m-Y H:i:s',
+        'updated_at' => 'date:d-m-Y H:i:s'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    
+    public function getReport($dari, $sampai,$stokdari, $stoksampai )
+    {
+        $getJudul = DB::table('parameter')
+        ->select('text')
+        ->where('grp', 'JUDULAN LAPORAN')
+        ->where('subgrp', 'JUDULAN LAPORAN')
+        ->first();
+
+       
+            $getPOStok = DB::table('parameter')
+            ->where('grp', 'SPB STOK')
+            ->where('subgrp', 'SPB STOK')
+            ->value('text');
+            $penerimaanstok_id = $getPOStok;
+
+       $result2 = DB::table('penerimaanstokheader AS a')
+                    ->select(
+                        'a.nobukti',
+                        'a.tglbukti',
+                        DB::raw("ISNULL(c.namasupplier, '') AS namasupplier"),
+                        'b.stok_id',
+                        'd.namastok',
+                        'b.qty',
+                        'b.harga',
+                        'b.nominaldiscount',
+                        DB::raw('(b.qty * b.harga - b.nominaldiscount) AS total'),
+                        DB::raw("ISNULL(e.satuan, '') AS satuan"),
+                        'b.keterangan',
+                    DB::raw("'Laporan Pembelian' as judulLaporan"),
+                    DB::raw("'" . $getJudul->text . "' as judul"),
+                    DB::raw("'Tgl Cetak :'+format(getdate(),'dd-MM-yyyy HH:mm:ss')as tglcetak"),
+                    DB::raw(" 'User :".auth('api')->user()->name."' as usercetak")
+                    )
+                    ->join('penerimaanstokdetail AS b', 'a.nobukti', '=', 'b.nobukti')
+                    ->leftJoin('supplier AS c', 'a.supplier_id', '=', 'c.id')
+                    ->join('stok AS d', 'b.stok_id', '=', 'd.id')
+                    ->leftJoin('satuan AS e', 'd.satuan_id', '=', 'e.id')
+                    ->where('a.tglbukti', '>=', $dari)
+                    ->where('a.tglbukti', '<=', $sampai)
+                    ->where('a.supplier_id', '>=', $stokdari)
+                    ->where('a.supplier_id', '<=', $stoksampai)
+                    ->where('a.penerimaanstok_id', '=', $penerimaanstok_id)
+                    ->orderBy('a.id')
+                    ->orderBy('b.id');
+                    
+                   
+        $data = $result2->get();
+        return $data;
+                
+
+       
+        
+}
+
+public function getExport($dari, $sampai,$stokdari, $stoksampai )
+{
+    $getJudul = DB::table('parameter')
+    ->select('text')
+    ->where('grp', 'JUDULAN LAPORAN')
+    ->where('subgrp', 'JUDULAN LAPORAN')
+    ->first();
+
+   
+        $getPOStok = DB::table('parameter')
+        ->where('grp', 'SPB STOK')
+        ->where('subgrp', 'SPB STOK')
+        ->value('text');
+        $penerimaanstok_id = $getPOStok;
+
+   $result2 = DB::table('penerimaanstokheader AS a')
+                ->select(
+                    'a.nobukti',
+                    'a.tglbukti',
+                    DB::raw("ISNULL(c.namasupplier, '') AS namasupplier"),
+                    'b.stok_id',
+                    'd.namastok',
+                    'b.qty',
+                    'b.harga',
+                    'b.nominaldiscount',
+                    DB::raw('(b.qty * b.harga - b.nominaldiscount) AS total'),
+                    DB::raw("ISNULL(e.satuan, '') AS satuan"),
+                    'b.keterangan'
+                )
+                ->join('penerimaanstokdetail AS b', 'a.nobukti', '=', 'b.nobukti')
+                ->leftJoin('supplier AS c', 'a.supplier_id', '=', 'c.id')
+                ->join('stok AS d', 'b.stok_id', '=', 'd.id')
+                ->leftJoin('satuan AS e', 'd.satuan_id', '=', 'e.id')
+                ->where('a.tglbukti', '>=', $dari)
+                ->where('a.tglbukti', '<=', $sampai)
+                ->where('a.supplier_id', '>=', $stokdari)
+                ->where('a.supplier_id', '<=', $stoksampai)
+                ->where('a.penerimaanstok_id', '=', $penerimaanstok_id)
+                ->orderBy('a.id')
+                ->orderBy('b.id');
+               
+        $data = $result2->get();
+        return $data;
+           
+            
+
+   
+    
+}
+}
