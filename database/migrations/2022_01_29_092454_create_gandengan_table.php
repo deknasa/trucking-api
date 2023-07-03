9b88242592fcc269c9e65000b3c43aeba4ae583a
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-07-03 13:12:46 +0700

    penambahan migration

--- database/migrations/2022_01_29_092454_create_gandengan_table.php
+++ database/migrations/2022_01_29_092454_create_gandengan_table.php
@@ -17,6 +17,8 @@ class CreateGandenganTable extends Migration
             $table->id();
             $table->string('kodegandengan', 300)->nullable();
             $table->string('keterangan', 300)->nullable();
+            $table->integer('jumlahroda')->length(11)->nullable();
+            $table->integer('jumlahbanserap')->length(11)->nullable();
             $table->integer('statusaktif')->length(11)->nullable();
             $table->string('modifiedby', 30)->nullable();            
             $table->timestamps();
