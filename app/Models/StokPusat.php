5100ff28d7a3c1fd49cc1c8f1e76d71922c541ac
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-07-06 10:12:44 +0700

    perbaikan migration

---
+++ app/Models/StokPusat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StokPusat extends Model
+{
+    use HasFactory;
+}
