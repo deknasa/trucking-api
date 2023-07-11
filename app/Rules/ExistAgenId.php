45223548bfb152766464193e2706c8b58929a736
bshf <bungashafa53@gmail.com>
2023-07-11 09:41:04 +0700

    fix laporankartupiutang per agen

---
+++ app/Rules/ExistAgenId.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Rules;
+
+use App\Http\Controllers\Api\ErrorController;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class ExistAgenId implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $agen = DB::table("agen")->from(DB::raw("agen with (readuncommitted)"))
+            ->where('id', $value)
+            ->first();
+        if ($agen == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        $controller = new ErrorController;
+        return ':attribute' . ' ' . $controller->geterror('TVD')->keterangan;
+    }
+}
