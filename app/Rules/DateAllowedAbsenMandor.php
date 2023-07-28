3f19dd2c8197ce4608a21b69a448b9c2fbe99d33
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-07-28 17:06:40 +0700

    validasi mandor edit absensi

---
+++ app/Rules/DateAllowedAbsenMandor.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use App\Models\BukaAbsensi;
+use App\Models\AbsensiSupirHeader;
+
+class DateAllowedAbsen implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($param)
+    {
+       
+    }
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $date = date('Y-m-d', strtotime($value));
+        $today = date('Y-m-d', strtotime("today"));
+        $allowed = false ;
+        $bukaAbsensi = BukaAbsensi::where('tglabsensi', '=', $date)->first();
+        if ($bukaAbsensi){
+            $allowed = true;
+        }
+        
+        
+        return $allowed ;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Tidak Bisa memilih tanggal tersebut';
+    }
+}
