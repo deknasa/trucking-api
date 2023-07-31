fd249e28a3ccfc9f02b7eb470e841717c7b2c5b9
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-07-31 09:39:48 +0700

    perbaikan migration

--- database/migrations/2022_09_24_000043_create_absensisupirdetail_table.php
+++ database/migrations/2022_09_24_000043_create_absensisupirdetail_table.php
@@ -23,15 +23,17 @@ class CreateabsensisupirdetailTable extends Migration
             $table->unsignedBigInteger('trado_id')->nullable();
             $table->unsignedBigInteger('supir_id')->nullable();
             $table->longText('keterangan')->nullable();
-            $table->double('uangjalan', 15,2)->nullable();
+            $table->double('uangjalan', 15, 2)->nullable();
             $table->unsignedBigInteger('absen_id')->nullable();
             $table->time('jam')->nullable();
-            
+            $table->integer('statusapprovaleditabsensi')->Length(11)->nullable();
+            $table->string('userapprovaleditabsensi', 50)->nullable();
+            $table->date('tglapprovaleditabsensi')->nullable();
             $table->string('modifiedby', 50)->nullable();
             $table->timestamps();
 
 
-            $table->foreign('absensi_id', 'absensisupirdetail_absensisupirheader_absensi_id_foreign')->references('id')->on('absensisupirheader')->onDelete('cascade');    
+            $table->foreign('absensi_id', 'absensisupirdetail_absensisupirheader_absensi_id_foreign')->references('id')->on('absensisupirheader')->onDelete('cascade');
             $table->foreign('trado_id', 'absensisupirdetail_trado_trado_id_foreign')->references('id')->on('trado');
             $table->foreign('supir_id', 'absensisupirdetail_supir_supir_id_foreign')->references('id')->on('supir');
             $table->foreign('absen_id', 'absensisupirdetail_absentrado_absen_id_foreign')->references('id')->on('absentrado');
