4a331c1c52dc997d83904846428f9f7810008c5b
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-08-02 14:10:59 +0700

    perbaikan seeder

---
+++ database/migrations/2023_08_02_132823_create_karyawanlogabsensi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawanlogabsensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawanlogabsensi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idabsen')->nullable();
+            $table->date('tglresign')->nullable();
+            $table->integer('statusaktif')->length(11)->nullable();            
+            $table->string('modifiedby', 30)->nullable();                    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawanlogabsensi');
+    }
+}
