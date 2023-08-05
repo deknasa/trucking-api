e8f6db9858bdf5f564ca166038c26cb950238a50
bshf <bungashafa53@gmail.com>
2023-08-05 13:34:07 +0700

    update dashboard

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -11,78 +11,17 @@ use App\Http\Requests\UpdateDashboardRequest;
 class DashboardController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @ClassName 
      */
     public function index()
     {
-        //
+        $dashboard = new Dashboard();
+
+
+
+        return response([
+            'data' => $dashboard->getTrado(),
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreDashboardRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreDashboardRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Dashboard  $dashboard
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Dashboard $dashboard)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Dashboard  $dashboard
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Dashboard $dashboard)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateDashboardRequest  $request
-     * @param  \App\Models\Dashboard  $dashboard
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateDashboardRequest $request, Dashboard $dashboard)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Dashboard  $dashboard
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Dashboard $dashboard)
-    {
-        //
-    }
 }
--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Dashboard extends MyModel
 {
     use HasFactory;
+
+    public function getTrado()
+    {
+        $aktif = DB::table("parameter")->from(DB::raw("parameter with (readuncommitted)"))->where('grp', 'STATUS AKTIF')->where("text", 'AKTIF')->first();
+        $nonAktif = DB::table("parameter")->from(DB::raw("parameter with (readuncommitted)"))->where('grp', 'STATUS AKTIF')->where("text", 'NON AKTIF')->first();
+
+        $tradoAktif = DB::table("trado")->from(DB::raw("trado with (readuncommitted)"))->where('statusaktif', $aktif->id)->count();
+        $tradoNonAktif = DB::table("trado")->from(DB::raw("trado with (readuncommitted)"))->where('statusaktif', $nonAktif->id)->count();
+        $supirAktif = DB::table("supir")->from(DB::raw("supir with (readuncommitted)"))->where('statusaktif', $aktif->id)->count();
+        $supirNonAktif = DB::table("supir")->from(DB::raw("supir with (readuncommitted)"))->where('statusaktif', $nonAktif->id)->count();
+
+        $data = [
+            'tradoaktif' => $tradoAktif,
+            'tradononaktif' => $tradoNonAktif,
+            'supiraktif' => $supirAktif,
+            'supirnonaktif' => $supirNonAktif,
+        ];
+        return $data;
+    }
 }
