17fcb896c0a6fddea771d9142c866dbaa1c7f506
syarifa20 <syaripah2004@gmail.com>
2023-08-22 11:07:31 +0700

    perbaikan reset password

--- app/Http/Controllers/Api/ForgotPasswordController.php
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -18,35 +18,41 @@ class ForgotPasswordController extends Controller
 {
     public function sendResetLink(ForgotPasswordRequest $request)
     {
-       
-
-        $user = User::where('user', $request->user)->first();
-
-        
-        $expirationInMinutes = config('app.jwt_exp');
-
-        $payload = [
-            'email' => strtolower($user->email),
-            'iat' => microtime(true),
-            'exp' => microtime(true) + ($expirationInMinutes * 60)
-        ];
-
-       
-        $token = JWT::encode($payload, config('app.jwt_key'), config('app.jwt_alg'));
-
-        $resetLink = config('app.web_url') . "reset-password/$token";
-        
-        $passwordReset = new PasswordReset();
-        $passwordReset->email = $user->email;
-        $passwordReset->token = $token;
-        $passwordReset->timestamps = false;
-
-        if ($passwordReset->save()) {
-            Mail::to($user->email)->send(new ResetPassword($resetLink));
-
+        if (isset($request->check) && $request->check == true) {
             return response([
-                'message' => "LINK RESET PASSWORD AKAN DIKIRIMKAN KE $user->email",
+                'message' => 'user exist'
             ]);
+        } else {
+
+
+            $user = User::where('user', $request->user)->first();
+
+            $expirationInMinutes = config('app.jwt_exp');
+
+            $payload = [
+                'email' => strtolower($user->email),
+                'iat' => microtime(true),
+                'exp' => microtime(true) + ($expirationInMinutes * 60)
+            ];
+
+
+            $token = JWT::encode($payload, config('app.jwt_key'), config('app.jwt_alg'));
+
+
+            $resetLink = config('app.web_url') . "reset-password/$token";
+
+            $passwordReset = new PasswordReset();
+            $passwordReset->email = $user->email;
+            $passwordReset->token = $token;
+            $passwordReset->timestamps = false;
+
+            if ($passwordReset->save()) {
+                Mail::to($user->email)->send(new ResetPassword($resetLink));
+
+                return response([
+                    'message' => "LINK RESET PASSWORD AKAN DIKIRIMKAN KE $user->email",
+                ]);
+            }
         }
     }
 
@@ -75,6 +81,4 @@ class ForgotPasswordController extends Controller
             throw $th;
         }
     }
-
-  
 }
