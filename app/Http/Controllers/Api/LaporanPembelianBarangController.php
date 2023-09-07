68fb99e21510fd6718cf8b9789d8691274187769
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-09-07 17:12:03 +0700

    perbaikan seeder

---
+++ app/Http/Controllers/Api/LaporanPembelianBarangController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\LaporanPembelianBarang;
+use App\Http\Requests\ValidasiLaporanPembelianBarangRequest;
+use App\Http\Requests\StoreLaporanPembelianBarangRequest;
+use App\Http\Requests\UpdateLaporanPembelianBarangRequest;
+
+class LaporanPembelianBarangController extends Controller
+{
+    /**
+     * @ClassName
+     */
+    public function index(Request $request)
+    {
+        return response([
+            'data' => [],
+            'attributes' => [
+                'totalRows' => 0,
+                'totalPages' => 0
+            ]
+        ]);
+    }
+
+     /**
+     * @ClassName
+     */
+    public function report(ValidasiLaporanPembelianBarangRequest $request)
+    {
+        $bulan = substr($request->sampai, 0, 2);
+        $tahun = substr($request->sampai, 3, 4);
+
+        $laporanpembelianbarang = new LaporanPembelianBarang();
+
+        $laporan_pembelianbarang = $laporanpembelianbarang->getReport($bulan, $tahun);
+
+        if (count($laporan_pembelianbarang) == 0) {
+            return response([
+                'data' => $laporan_pembelianbarang,
+                'message' => 'tidak ada data'
+            ], 500);
+        }else{
+            return response([
+                'data' => $laporan_pembelianbarang,
+                'message' => 'berhasil'
+            ]);
+        }
+        
+    }
+
+    /**
+     * @ClassName
+     */
+    public function export(Request $request)
+    {
+        $bulan = $request->bulan;
+        $tahun = $request->tahun;
+
+        $laporanpembelianbarang = new LaporanPembelianBarang();
+
+
+        $laporan_pembelianbarang = $laporanpembelianbarang->getReport($bulan, $tahun);
+        // foreach($laporan_pembelianbarang as $item){
+        //     $item->tglbukti = date('d-m-Y', strtotime($item->tglbukti));
+        //     $item->tgljatuhtempo = date('d-m-Y', strtotime($item->tgljatuhtempo));
+        // }
+
+        return response([
+            'data' => $laporan_pembelianbarang
+            // 'data' => $report
+        ]);
+    }
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLaporanPembelianBarangRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLaporanPembelianBarangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LaporanPembelianBarang  $laporanPembelianBarang
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LaporanPembelianBarang $laporanPembelianBarang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LaporanPembelianBarang  $laporanPembelianBarang
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LaporanPembelianBarang $laporanPembelianBarang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateLaporanPembelianBarangRequest  $request
+     * @param  \App\Models\LaporanPembelianBarang  $laporanPembelianBarang
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateLaporanPembelianBarangRequest $request, LaporanPembelianBarang $laporanPembelianBarang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LaporanPembelianBarang  $laporanPembelianBarang
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LaporanPembelianBarang $laporanPembelianBarang)
+    {
+        //
+    }
+}
---
+++ app/Models/LaporanPemakaianStok.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class LaporanPemakaianStok extends MyModel
+{
+    use HasFactory;
+
+    
+    protected $table = '';
+
+    protected $casts = [
+        'created_at' => 'date:d-m-Y H:i:s',
+        'updated_at' => 'date:d-m-Y H:i:s'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function getReport($bulan, $tahun)
+    {
+
+
+        $getJudul = DB::table('parameter')
+            ->select('text')
+            ->where('grp', 'JUDULAN LAPORAN')
+            ->where('subgrp', 'JUDULAN LAPORAN')
+            ->first();
+
+
+        $cmpy = DB::table('parameter')
+            ->select('text')
+            ->where('grp', 'JUDULAN LAPORAN')
+            ->where('subgrp', 'JUDULAN LAPORAN')
+            ->value('text');
+
+        $Temprekappendapatan = '##Temprekappendapatan' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
+        Schema::create($Temprekappendapatan, function ($table) {
+            $table->string('coamain', 30);
+            $table->double('nominal');
+        });
+
+        $select_Temprekappendapatan = DB::table('jurnalumumpusatdetail')->from(DB::raw("jurnalumumpusatdetail AS D WITH (READUNCOMMITTED)"))
+
+            ->select(
+                'D.coamain',
+                DB::raw('SUM(-D.Nominal)')
+            )
+
+            ->join(DB::raw("jurnalumumpusatheader as H with (readuncommitted)"), 'H.nobukti', '=', 'D.nobukti')
+            ->join('mainakunpusat as CD', 'CD.COA', '=', 'D.coamain')
+            ->whereRaw("MONTH(D.tglbukti) = " . $bulan . " AND YEAR(D.tglbukti) = " . $tahun)
+            ->groupBy('D.coamain');
+
+        // dd("Adas");
+        DB::table($Temprekappendapatan)->insertUsing([
+            'coamain',
+            'nominal',
+        ], $select_Temprekappendapatan);
+        // dd($select_Temprekappendapatan->get());
+
+        $TempLabaRugi = '##TempLabaRugi' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
+        Schema::create($TempLabaRugi, function ($table) {
+            $table->bigIncrements('id');
+            $table->string('keteranganmain', 500);
+            $table->integer('ordermain');
+            $table->string('type', 1000);
+            $table->string('coa', 100);
+            $table->string('keterangancoa', 1000);
+            $table->double('nominal');
+            $table->string('cmpyname', 300);
+            $table->integer('statuslabarugi');
+            $table->integer('bln');
+            $table->integer('thn');
+            $table->integer('order');
+            $table->string('parent', 30);
+            $table->string('KeteranganParent', 1000);
+            $table->string('diperiksa', 1000);
+            $table->string('disetujui', 1000);
+        });
+
+
+        //    $cmpy = 'PT. TRANSPORINDO AGUNG SEJAHTERA';
+
+
+        $disetujui = db::table('parameter')->from(db::raw('parameter with (readuncommitted)'))
+            ->select('text')
+            ->where('grp', 'DISETUJUI')
+            ->where('subgrp', 'DISETUJUI')->first()->text ?? '';
+
+        $diperiksa = db::table('parameter')->from(db::raw('parameter with (readuncommitted)'))
+            ->select('text')
+            ->where('grp', 'DIPERIKSA')
+            ->where('subgrp', 'DIPERIKSA')->first()->text ?? '';
+
+
+        $results = DB::table('mainakunpusat AS C')
+            ->select(
+                DB::raw("'PENDAPATAN :' AS keteranganmain"),
+                DB::raw('1 AS ordermain'),
+                'AT.kodeType AS type',
+                'C.COA AS coa',
+                'C.keterangancoa',
+                DB::raw('ISNULL(E.nominal, 0) AS Nominal'),
+                DB::raw("'$cmpy' AS CmpyName"),
+                'C.statuslabarugi',
+                DB::raw("'$bulan' AS bulan"),
+                DB::raw("'$tahun' AS tahun"),
+                'AT.Order',
+                'C.Parent',
+                DB::raw("ISNULL(G.keterangancoa, '') AS KeteranganParent"),
+                db::raw("'" . $disetujui . "' as disetujui"),
+                db::raw("'" . $diperiksa . "' as diperiksa"),
+
+
+            )
+            ->join('mainTypeakuntansi AS AT', 'AT.id', '=', 'C.type_id')
+            ->leftJoin('mainakunpusat AS G', 'C.parent', '=', 'G.coa')
+            ->leftJoin($Temprekappendapatan . ' AS E', 'C.coa', '=', 'E.CoaMAin')
+            ->whereIn('AT.kodetype', ['Pendapatan'])
+            ->whereRaw("isnull(E.nominal,0)<>0")
+            ->orderBy('coa');
+
+        // dd($results->toSql());
+
+
+        DB::table($TempLabaRugi)->insertUsing([
+            'keteranganmain',
+            'ordermain',
+            'type',
+            'coa',
+            'keterangancoa',
+            'nominal',
+            'cmpyname',
+            'statuslabarugi',
+            'bln',
+            'thn',
+            'order',
+            'parent',
+            'KeteranganParent',
+            'diperiksa',
+            'disetujui',
+        ], $results);
+        // dd($results->get()); 
+
+        $results2 = DB::table('mainakunpusat AS C')
+            ->select(
+                DB::raw("'BIAYA - BIAYA :' AS keteranganmain"),
+                DB::raw('2 AS OrderMain'),
+                'AT.kodeType AS type',
+                'C.COA AS coa',
+                'C.keterangancoa',
+                DB::raw('ISNULL(E.nominal, 0) AS Nominal'),
+                DB::raw("'$cmpy' AS CmpyName"),
+                'C.statuslabarugi',
+                DB::raw("'$bulan' AS bulan"),
+                DB::raw("'$tahun' AS tahun"),
+                'AT.Order',
+                'C.Parent',
+                DB::raw("ISNULL(G.keterangancoa, '') AS KeteranganParent"),
+                db::raw("'" . $disetujui . "' as disetujui"),
+                db::raw("'" . $diperiksa . "' as diperiksa"),
+
+            )
+            ->join('mainTypeakuntansi AS AT', 'AT.id', '=', 'C.type_id')
+            ->leftJoin('mainakunpusat AS G', 'C.parent', '=', 'G.coa')
+            ->leftJoin($Temprekappendapatan . ' AS E', 'C.coa', '=', 'E.CoaMAin')
+            ->whereIn('AT.kodetype', ['Beban'])
+            ->whereRaw("isnull(E.nominal,0)<>0");
+
+        DB::table($TempLabaRugi)->insertUsing([
+            'keteranganmain',
+            'ordermain',
+            'type',
+            'coa',
+            'keterangancoa',
+            'nominal',
+            'cmpyname',
+            'statuslabarugi',
+            'bln',
+            'thn',
+            'order',
+            'parent',
+            'KeteranganParent',
+            'diperiksa',
+            'disetujui',
+        ], $results2);
+
+        $data1 = $results->get();
+        $data2 = $results2->get();
+
+        $mergedData = $data1->concat($data2);
+        // return [$data1, $data2];
+        return $mergedData;
+    }
+
+}
