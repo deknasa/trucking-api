67421bf808f24aa4898b0f87004dabec6646e3ee
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-09-20 13:35:49 +0700

    info table

--- app/Models/JenisTrado.php
+++ app/Models/JenisTrado.php
@@ -274,7 +274,7 @@ class JenisTrado extends MyModel
         $jenistrado->kodejenistrado = $data['kodejenistrado'];
         $jenistrado->keterangan = $data['keterangan'] ?? '';
         $jenistrado->statusaktif =  $data['statusaktif'];
-        $jenistrado->modifiedby = auth('api')->user()->name;
+        $jenistrado->info = html_entity_decode(request()->info);
 
         if (!$jenistrado->save()) {
             throw new \Exception('Error updating jenis trado.');
