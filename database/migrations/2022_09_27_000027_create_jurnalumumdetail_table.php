a7231f3271838e960a9f9726bf819281169f788f
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-10-10 08:10:01 +0700

    perbaikan stok

--- database/migrations/2022_09_27_000027_create_jurnalumumdetail_table.php
+++ database/migrations/2022_09_27_000027_create_jurnalumumdetail_table.php
@@ -26,6 +26,8 @@ class CreateJurnalumumdetailTable extends Migration
             $table->double('nominal',15,2)->nullable();
             $table->longText('keterangan')->nullable();
             $table->integer('baris')->length(11)->nullable();
+            $table->string('hutangprediksi_nobukti',50)->nullable();
+            $table->double('hutangprediksi_nominal')->nullable();
             $table->longText('info')->nullable();            
             $table->string('modifiedby',50)->nullable();
             $table->timestamps();
