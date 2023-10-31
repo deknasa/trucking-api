51835c884208bef6dd2b09ad31f09dad3842fcdb
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-10-31 09:03:49 +0700

    perbaikan seeder

---
+++ database/migrations/2023_10_31_083935_create_tripinap_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripinapTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tripinap', function (Blueprint $table) {
+            $table->id();
+            $table->date ('tglabsensi')->nullable();
+            $table->unsignedBigInteger('trado_id')->nullable();
+            $table->unsignedBigInteger('supir_id')->nullable();
+            $table->string('suratpengantar_nobukti', 50)->nullable();              
+            $table->Time('jammasukinap')->nullable();
+            $table->Time('jamkeluarinap')->nullable();
+            $table->integer('statusapproval')->Length(11)->nullable();
+            $table->date('tglapproval')->nullable();
+            $table->string('userapproval',50)->nullable();
+            $table->longText('info')->nullable();
+            $table->string('modifiedby', 50)->nullable();              
+            $table->timestamps();
+        });
+        
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tripinap');
+    }
+}
---
+++ database/migrations/2023_10_31_084101_create_pengajuantripinap_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengajuantripinapTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuantripinap', function (Blueprint $table) {
+            $table->id();
+            $table->date ('tglabsensi')->nullable();
+            $table->unsignedBigInteger('trado_id')->nullable();
+            $table->unsignedBigInteger('supir_id')->nullable();
+            $table->integer('statusapproval')->Length(11)->nullable();
+            $table->date('tglapproval')->nullable();
+            $table->string('userapproval',50)->nullable();
+            $table->longText('info')->nullable();
+            $table->string('modifiedby', 50)->nullable();              
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuantripinap');
+    }
+}
