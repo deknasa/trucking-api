7c29b821082a780283019fad5c513a2348625249
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-11-02 09:56:29 +0700

    status ban sstok kalau tidak ban

--- app/Models/PengeluaranStokHeader.php
+++ app/Models/PengeluaranStokHeader.php
@@ -1431,7 +1431,7 @@ class PengeluaranStokHeader extends MyModel
                 $datastok  = Stok::lockForUpdate()->where("id", $data['detail_stok_id'][$i])
                     ->firstorFail();
                 $datastok->totalvulkanisir = $totalvulkan;
-                $datastok->statusban = $data['detail_statusban'][$i];
+                $datastok->statusban = ($data['detail_statusban']) ? $data['detail_statusban'][$i]: null;
                 $datastok->save();
             }
             // end update vulkanisir
@@ -2076,7 +2076,7 @@ class PengeluaranStokHeader extends MyModel
                 $datastok  = Stok::lockForUpdate()->where("id", $data['detail_stok_id'][$i])
                     ->firstorFail();
                 $datastok->totalvulkanisir = $totalvulkan;
-                $datastok->statusban = $data['detail_statusban'][$i];
+                $datastok->statusban = ($data['detail_statusban']) ? $data['detail_statusban'][$i]: null;
                 $datastok->save();
             }
             // end update vulkanisir
