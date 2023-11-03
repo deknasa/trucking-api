778eb322d0067299efa26a4501d066bba96c8a0d
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-11-03 17:00:33 +0700

    perbaikan seeder

--- app/Http/Controllers/Api/StatusOliTradoController.php
+++ app/Http/Controllers/Api/StatusOliTradoController.php
@@ -12,9 +12,20 @@ class StatusOliTradoController extends Controller
      */
     public function index()
     {
+   
+        $tgldari=date('Y-m-d',strtotime(request()->dari)) ?? '1900-01-01';
+        $tglsampai=date('Y-m-d',strtotime(request()->sampai)) ?? '1900-01-01';
+        $trado_id=request()->trado_id ?? 0;
+
+        if (request()->status=='all') {
+            $statusoli=0;
+        } else {
+            $statusoli=request()->status ?? 0;
+        }
+
         $statusOli = new StatusOliTrado();
         return response([
-            'data' => $statusOli->get(),
+            'data' => $statusOli->get($tgldari, $tglsampai,$trado_id,$statusoli),
             'attributes' => [
                 'totalRows' => $statusOli->totalRows,
                 'totalPages' => $statusOli->totalPages
