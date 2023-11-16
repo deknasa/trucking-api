82cdc1a8caefd81f51847d80e8af7eb522b2ae7b
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-11-16 13:05:54 +0700

    perbaikan seeder

--- app/Models/LaporanRekapTitipanEmkl.php
+++ app/Models/LaporanRekapTitipanEmkl.php
@@ -88,6 +88,96 @@ class LaporanRekapTitipanEmkl  extends MyModel
             'nominal',
         ], $querybiayatitipan);
 
+        $queryinvoiceextra=db::table("invoiceextraheader")->from(db::raw("invoiceextraheader a with (readuncommitted)"))
+        ->select(
+            'c.nobukti as nobukti',
+            'a.nobukti as nobuktiinvoice',
+            db::raw("max(c.tglbukti) as tglbukti"),
+            db::raw("max(b.keterangan) as keterangan"),
+            db::raw("sum(b.nominal) as nominal"),
+        )
+        ->join(db::raw("invoiceextradetail b with (readuncommitted)"),'a.nobukti','b.nobukti')
+        ->join(db::raw("piutangheader c with (readuncommitted)"),'a.piutang_nobukti','c.nobukti')
+        ->whereRaw("c.tglbukti<='" . $tanggal . "'")
+        ->groupBy('c.nobukti')
+        ->groupBy('a.nobukti');
+
+        $tempinvoiceextra = '##tempinvoiceextra' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
+
+        Schema::create($tempinvoiceextra, function ($table) {
+            $table->string('nobukti', 1000)->nullable();
+            $table->string('nobuktiinvoice', 1000)->nullable();
+            $table->date('tglbukti')->nullable();
+            $table->longText('keterangan')->nullable();
+            $table->double('nominal', 15, 2)->nullable();
+        });
+
+        DB::table($tempinvoiceextra)->insertUsing([
+            'nobukti',
+            'nobuktiinvoice',
+            'tglbukti',
+            'keterangan',
+            'nominal',
+        ], $queryinvoiceextra);
+
+        $temppelunasan = '##temppelunasan' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
+
+        Schema::create($temppelunasan, function ($table) {
+            $table->string('nobukti', 1000)->nullable();
+            $table->double('nominal', 15, 2)->nullable();
+        });        
+
+        $querypelunasan=db::table("pelunasanpiutangheader")->from(db::raw("pelunasanpiutangheader a with (readuncommitted)"))
+        ->select(
+            'b.piutang_nobukti as nobukti',
+            db::raw("sum(b.nominal) as nominal"),
+        )
+        ->join(db::raw("pelunasanpiutangdetail b with (readuncommitted)"),'a.nobukti','b.nobukti')
+        ->join(db::raw($tempinvoiceextra ." c with (readuncommitted)"),'b.piutang_nobukti','c.nobukti')
+        ->whereRaw("a.tglbukti<='" . $tanggal . "'")
+        ->groupBy('b.piutang_nobukti');
+
+
+        DB::table($temppelunasan)->insertUsing([
+            'nobukti',
+            'nominal',
+        ], $querypelunasan);
+
+
+
+
+        $tempsisainvoiceextra = '##tempsisainvoiceextra' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
+
+        Schema::create($tempsisainvoiceextra, function ($table) {
+            $table->string('nobukti', 1000)->nullable();
+            $table->string('nobuktiinvoice', 1000)->nullable();
+            $table->date('tglbukti')->nullable();
+            $table->longText('keterangan')->nullable();
+            $table->double('nominal', 15, 2)->nullable();
+        });        
+
+
+        $querysisa=db::table($tempinvoiceextra)->from(db::raw( $tempinvoiceextra ." a "))
+        ->select(
+            'a.nobuktiinvoice as nobukti',
+            'a.tglbukti',
+            'a.keterangan',
+            db::raw("0 as jenisorder_id"),
+            db::raw("(isnull(a.nominal,0)-isnull(b.nominal,0)) as nominal"),
+        )
+        ->leftjoin(db::raw($temppelunasan ." b "),'a.nobukti','b.nobukti')
+        ->whereRaw("(isnull(a.nominal,0)-isnull(b.nominal,0))<>0");
+
+        // dd( $querysisa->get());
+
+        // DB::table($tempbiayatitipan)->insertUsing([
+        //     'nobukti',
+        //     'tglbukti',
+        //     'keterangan',
+        //     'jenisorder_id',
+        //     'nominal',
+        // ], $querysisa);
+
         
         $getJudul = DB::table('parameter')
             ->select('text')
@@ -108,7 +198,7 @@ class LaporanRekapTitipanEmkl  extends MyModel
                 'a.nominal',
                 'b.keterangan as jenisorder'
             )
-            ->join(DB::raw("jenisorder as b with (readuncommitted) "), 'a.jenisorder_id', 'b.id')
+            ->leftjoin(DB::raw("jenisorder as b with (readuncommitted) "), 'a.jenisorder_id', 'b.id')
             ->orderBy('a.tglbukti', 'asc')
             ->orderBy('a.nobukti', 'asc');
 
