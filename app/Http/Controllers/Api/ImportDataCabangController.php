0a2d9e5c4b8365a03d935fcb5ee7291b5bc8aa43
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-11-17 14:06:42 +0700

    test koneksi

--- app/Http/Controllers/Api/ImportDataCabangController.php
+++ app/Http/Controllers/Api/ImportDataCabangController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use PDOException;
 use Illuminate\Http\Request;
 use App\Models\ImportDataCabang;
 use Illuminate\Support\Facades\DB;
@@ -42,4 +43,35 @@ class ImportDataCabangController extends Controller
             throw $th;
         }
     }
+    
+    public function testkoneksi() {
+        try {
+            
+            $test = (new ImportDataCabang())->testkoneksi();
+            return response()->json([
+                'message' => 'Berhasil disimpan',
+                'data' => $test
+            ], 201);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            $test = "Database asdfasdfasdfasfasdfasdfsadfdas asdf asdf asd fasd fad asd't work <br>$th";
+        }
+    
+        return $test;
+    }
+    public function xxx() {
+        try {
+            $dbconnect = DB::connection('sqlsrv2')->getPDO();
+            
+            $dbname = DB::connection('sqlsrv2')->getDatabaseName();
+                # code...
+            echo "Connected successfully to the database. Database name is :".$dbname;
+        } catch (\Exception $e) {
+            $test = "Database doesn't work";
+        }
+    
+        return $test;
+    }
+
+    
 }
