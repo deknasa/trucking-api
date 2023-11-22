2bd338747e0dbb779cba4e20143705984e6e4c2f
bshf <bungashafa53@gmail.com>
2023-11-22 17:07:38 +0700

    fix edit jurnalumum

--- app/Http/Controllers/Api/JurnalUmumHeaderController.php
+++ app/Http/Controllers/Api/JurnalUmumHeaderController.php
@@ -530,20 +530,44 @@ class JurnalUmumHeaderController extends Controller
         $statusCetak = Parameter::from(DB::raw("parameter with (readuncommitted)"))
             ->where('grp', 'STATUSCETAK')->where('text', 'CETAK')->first();
         $aksi = request()->aksi ?? '';
+        $tutupBuku = DB::table("parameter")->from(DB::raw("parameter with (readuncommitted)"))->where('grp', 'TUTUP BUKU')->first();
 
         if ($status == $statusApproval->id && ($aksi == 'DELETE' || $aksi == 'EDIT')) {
-            $query = DB::table('error')
-                ->select('keterangan')
-                ->where('kodeerror', '=', 'SAP')
-                ->get();
-            $keterangan = $query['0'];
-            $data = [
-                'message' => $keterangan,
-                'errors' => 'sudah approve',
-                'kodestatus' => '1',
-                'kodenobukti' => '1'
-            ];
+            if ($jurnalumum->statusformat == 115) {
+                if (date('Y-m-d', strtotime($jurnalumum->tglbukti)) < date('Y-m-d', strtotime($tutupBuku->text))) {
+                    $query = DB::table('error')
+                        ->select('keterangan')
+                        ->where('kodeerror', '=', 'STB')
+                        ->get();
+                    $keterangan = $query['0'];
+                    $data = [
+                        'message' => $keterangan,
+                        'errors' => 'sudah tutup buku',
+                        'kodestatus' => '1',
+                        'kodenobukti' => '1'
+                    ];
+                } else {
+                    $data = [
+                        'message' => '',
+                        'errors' => 'belum approve',
+                        'kodestatus' => '0',
+                        'kodenobukti' => '1'
+                    ];
+                }
+            } else {
 
+                $query = DB::table('error')
+                    ->select('keterangan')
+                    ->where('kodeerror', '=', 'SAP')
+                    ->get();
+                $keterangan = $query['0'];
+                $data = [
+                    'message' => $keterangan,
+                    'errors' => 'sudah approve',
+                    'kodestatus' => '1',
+                    'kodenobukti' => '1'
+                ];
+            }
             return response($data);
         } else if ($statusdatacetak == $statusCetak->id) {
             $query = DB::table('error')
