905108f8e120d99dfe12991721b1f6518fe44d47
ryan ryan <ryan_vixy1402@yahoo.com>
2023-12-02 05:56:25 +0700

    perbaikan seeder

--- app/Models/InvoiceLunasKePusat.php
+++ app/Models/InvoiceLunasKePusat.php
@@ -29,7 +29,6 @@ class InvoiceLunasKePusat extends MyModel
 
         $periode = request()->periode ?? '01-1900';
         $this->setRequestParameters();
-
         $tempinvoice = '##tempinvoice' . rand(1, getrandmax()) . str_replace('.', '', microtime(true));
         Schema::create($tempinvoice, function ($table) {
             $table->id();
@@ -79,8 +78,10 @@ class InvoiceLunasKePusat extends MyModel
                 db::raw("a.nominal as sisa"),
             )
             ->leftjoin(db::raw($tempinvoice . " b "), 'a.nobukti', 'b.nobukti')
-            ->whereRaw("isnull(a.nobukti,'')=''")
+            ->whereRaw("isnull(b.nobukti,'')=''")
             ->whereRaw("format(a.tglbukti,'MM-yyyy')='" . $periode . "'");
+
+            // dd($querytemp->tosql());
 
         DB::table($tempinvoice)->insertUsing([
             'invoiceheader_id',
@@ -98,16 +99,17 @@ class InvoiceLunasKePusat extends MyModel
                 'a.invoiceheader_id',
                 'a.nobukti',
                 'a.tglbukti',
-                'a.agen_id',
+                db::raw("isnull(b.kodeagen,'') as agen_id"),
                 'a.nominal',
                 'a.tglbayar',
                 'a.bayar',
                 'a.sisa',
             )
+            ->leftjoin(db::raw("agen  b with (readuncommitted)"),'a.agen_id','b.id')
             ->orderBy('a.nobukti', 'asc');
 
 
-            dd($query->get());
+            // dd($query->get());
         $this->filter($query);
         $this->totalRows = $query->count();
         $this->totalPages = request()->limit > 0 ? ceil($this->totalRows / request()->limit) : 1;
@@ -117,6 +119,10 @@ class InvoiceLunasKePusat extends MyModel
         // dd($this->totalPages);
         return $data;
     }
+    public function paginate($query)
+    {
+        return $query->skip($this->params['offset'])->take($this->params['limit']);
+    }
 
     public function filter($query, $relationFields = [])
     {
@@ -124,9 +130,14 @@ class InvoiceLunasKePusat extends MyModel
             switch ($this->params['filters']['groupOp']) {
                 case "AND":
                     foreach ($this->params['filters']['rules'] as $index => $filters) {
-
-                        // $query = $query->where($this->table . '.' . $filters['field'], 'LIKE', "%$filters[data]%");
-                        $query = $query->whereRaw("a.[" .  $filters['field'] . "] LIKE '%" . escapeLike($filters['data']) . "%' escape '|'");
+                        if ($filters['field'] != '') {
+                            if ($filters['field'] == 'agen_id') {
+                                $query = $query->where('b.kodeagen', '=', $filters['data']);
+                            } else {
+                                // $query = $query->where($this->table . '.' . $filters['field'], 'LIKE', "%$filters[data]%");
+                                $query = $query->whereRaw("a.[" .  $filters['field'] . "] LIKE '%" . escapeLike($filters['data']) . "%' escape '|'");
+                            }
+                        }
                     }
 
                     break;
@@ -134,8 +145,14 @@ class InvoiceLunasKePusat extends MyModel
                     $query = $query->where(function ($query) {
 
                         foreach ($this->params['filters']['rules'] as $index => $filters) {
+                            if ($filters['field'] != '') {
+                                if ($filters['field'] == 'agen_id') {
+                                    $query = $query->orWhere('b.kodeagen', '=', $filters['data']);
+                                } else {
+                                    $query = $query->orWhereRaw( "a.[" .  $filters['field'] . "] LIKE '%" . escapeLike($filters['data']) . "%' escape '|'");
+                                }
+                            }
 
-                            $query = $query->orWhereRaw( "a.[" .  $filters['field'] . "] LIKE '%" . escapeLike($filters['data']) . "%' escape '|'");
                         }
                     });
 
@@ -157,4 +174,99 @@ class InvoiceLunasKePusat extends MyModel
        
         return $query->orderBy('a.'.$this->params['sortIndex'], $this->params['sortOrder']);
     }
+
+    public function getinvoicelunas($id)
+    {
+        $query = db::table("invoiceheader")->from(db::raw("invoiceheader a with (readuncommitted)"))
+        ->select(
+            'a.id as invoiceheader_id',
+            'a.nobukti',
+            'a.tglbukti',
+            db::raw("isnull(b.kodeagen,'') as agen"),
+            db::raw("isnull(b.id,0) as agen_id"),
+            'a.nominal',
+            db::raw("isnull(c.tglbayar,format(getdate(),'yyyy/MM/dd')) as tglbayar"),
+            db::raw("isnull(c.bayar,0) as bayar"),
+            db::raw("(isnull(a.nominal,0)-isnull(c.nominal,0)) as sisa"),
+        )
+        ->leftjoin(db::raw("agen b with (readuncommitted)"),'a.agen_id','b.id')
+        ->leftjoin(db::raw("invoicelunaskepusat c with (readuncommitted)"),'a.nobukti','c.nobukti')
+        ->where('a.id',$id);
+
+
+        return $query->first();
+    }
+
+    public function processStore(array $data)
+    {
+        $InvoiceLunaskePusat = new InvoiceLunasKePusat();
+        $InvoiceLunaskePusat->invoiceheader_id = $data['invoiceheader_id'];
+        $InvoiceLunaskePusat->nobukti = $data['nobukti'] ?? '';
+        $InvoiceLunaskePusat->tglbukti =$data['tglbukti'];
+        $InvoiceLunaskePusat->agen_id =$data['agen_id'];
+        $InvoiceLunaskePusat->nominal =$data['nominal'];
+        $InvoiceLunaskePusat->tglbayar =$data['tglbayar'];
+        $InvoiceLunaskePusat->bayar =$data['bayar'];
+        $InvoiceLunaskePusat->sisa =$data['sisa'];
+        $InvoiceLunaskePusat->modifiedby = auth('api')->user()->name;
+        $InvoiceLunaskePusat->info = html_entity_decode(request()->info);
+        // $request->sortname = $request->sortname ?? 'id';
+        // $request->sortorder = $request->sortorder ?? 'asc';
+
+        if (!$jenisorder->save()) {
+            throw new \Exception("Error Simpan Invoice Lunas ke Pusat");
+        }
+
+        (new LogTrail())->processStore([
+            'namatabel' => strtoupper($InvoiceLunaskePusat->getTable()),
+            'postingdari' => 'ENTRY Invoice Lunas ke Pusat',
+            'idtrans' => $InvoiceLunaskePusat->id,
+            'nobuktitrans' => $InvoiceLunaskePusat->id,
+            'aksi' => 'ENTRY',
+            'datajson' => $InvoiceLunaskePusat->toArray(),
+            'modifiedby' => $InvoiceLunaskePusat->modifiedby
+        ]);
+
+        return $InvoiceLunaskePusat;
+    }
+    public function processUpdate(InvoiceLunaskePusat $InvoiceLunaskePusat, array $data)
+    {
+
+        $InvoiceLunaskePusat->invoiceheader_id = $data['invoiceheader_id'];
+        $InvoiceLunaskePusat->nobukti = $data['nobukti'] ?? '';
+        $InvoiceLunaskePusat->tglbukti =$data['tglbukti'];
+        $InvoiceLunaskePusat->agen_id =$data['agen_id'];
+        $InvoiceLunaskePusat->nominal =$data['nominal'];
+        $InvoiceLunaskePusat->tglbayar =$data['tglbayar'];
+        $InvoiceLunaskePusat->bayar =$data['bayar'];
+        $InvoiceLunaskePusat->sisa =$data['sisa'];
+        $InvoiceLunaskePusat->modifiedby = auth('api')->user()->name;
+        $InvoiceLunaskePusat->info = html_entity_decode(request()->info);
+
+
+        if (!$jenisorder->save()) {
+            throw new \Exception("Error update Invoice Lunas ke Pusat.");
+        }
+        (new LogTrail())->processStore([
+            'namatabel' => strtoupper($InvoiceLunaskePusat->getTable()),
+            'postingdari' => 'EDIT Invoice Lunas ke Pusat',
+            'idtrans' => $InvoiceLunaskePusat->id,
+            'nobuktitrans' => $InvoiceLunaskePusat->id,
+            'aksi' => 'EDIT',
+            'datajson' => $InvoiceLunaskePusat->toArray(),
+            'modifiedby' => $InvoiceLunaskePusat->modifiedby
+        ]);
+
+        return $jenisorder;
+    }
+
+
+    public function processDestroy($id)
+    {
+        // $AbsensiSupirHeader = AbsensiSupirHeader::where('id', $AbsensiSupirDetail->absensi_id)->first();
+        $InvoiceLunaskePusat = InvoiceLunasKePusat::where('id', $id)->lockForUpdate()->first();
+        $InvoiceLunaskePusat->delete();
+        return $InvoiceLunaskePusat;
+    }
+
 }
