81d2f7d8c980076c2e8af01c73d45e94b2e9b7c1
bshf <bungashafa53@gmail.com>
2023-12-15 09:16:51 +0700

    fix validasi penerimaantruckingheader

--- app/Http/Requests/StorePenerimaanTruckingHeaderRequest.php
+++ app/Http/Requests/StorePenerimaanTruckingHeaderRequest.php
@@ -175,13 +175,11 @@ class StorePenerimaanTruckingHeaderRequest extends FormRequest
                     'required',
                     'date_format:d-m-Y',
                     'before_or_equal:' . date('d-m-Y'),
-                    new DateTutupBuku(),
                 ],
                 'periodesampai' => [
                     'required',
                     'date_format:d-m-Y',
                     'before_or_equal:' . date('d-m-Y'),
-                    new DateTutupBuku(),
                 ],
                 'keteranganheader' => 'required'
             ];
