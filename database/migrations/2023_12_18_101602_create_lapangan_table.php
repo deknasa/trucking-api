de41a8edc8736b612d5317f254753f9b35cb3185
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-12-18 10:27:05 +0700

    perbaikan seeder

---
+++ database/migrations/2023_12_18_101602_create_lapangan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLapanganTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lapangan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('agen_id')->nullable();
+            $table->unsignedBigInteger('container_id')->nullable();
+            $table->double('nominal',15,2)->nullable();   
+            $table->longText('info')->nullable();
+            $table->string('modifiedby',50)->nullable();     
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lapangan');
+    }
+}
