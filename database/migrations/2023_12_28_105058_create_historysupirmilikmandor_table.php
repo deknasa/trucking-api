1b647f14542f63679db2c7bb885a076a7b4bb843
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-12-28 10:58:01 +0700

    perbaikan seeder

---
+++ database/migrations/2023_12_28_105058_create_historysupirmilikmandor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistorysupirmilikmandorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historysupirmilikmandor', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supir_id')->nullable();
+            $table->unsignedBigInteger('mandor_id')->nullable();
+            $table->date('tglberlaku')->nullable();
+            $table->longText('info')->nullable();
+            $table->string('modifiedby', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historysupirmilikmandor');
+    }
+}
---
+++ database/migrations/2023_12_28_105143_create_historytradomilikmandor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistorytradomilikmandorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historytradomilikmandor', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('trado_id')->nullable();
+            $table->unsignedBigInteger('mandor_id')->nullable();
+            $table->date('tglberlaku')->nullable();
+            $table->longText('info')->nullable();
+            $table->string('modifiedby', 50)->nullable();            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historytradomilikmandor');
+    }
+}
