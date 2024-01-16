abaa21b2ee9626547b233e9f287f5e938f246d80
bshf <bungashafa53@gmail.com>
2024-01-16 11:35:07 +0700

    move process from controller to model app tradogambar&keterangan

--- app/Models/ApprovalTradoGambar.php
+++ app/Models/ApprovalTradoGambar.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 
 class ApprovalTradoGambar extends MyModel
 {
@@ -21,16 +22,16 @@ class ApprovalTradoGambar extends MyModel
     {
         $this->setRequestParameters();
         $query = DB::table($this->table)->from(DB::raw("$this->table with (readuncommitted)"))
-        ->select(
-            'approvaltradogambar.id',
-            'approvaltradogambar.kodetrado',
-            'parameter.memo as statusapproval',
-            'approvaltradogambar.tglbatas',
-            'approvaltradogambar.created_at',
-            'approvaltradogambar.updated_at',
-            'approvaltradogambar.modifiedby'
-        )
-        ->join(DB::raw("parameter with (readuncommitted)"), 'approvaltradogambar.statusapproval', 'parameter.id');
+            ->select(
+                'approvaltradogambar.id',
+                'approvaltradogambar.kodetrado',
+                'parameter.memo as statusapproval',
+                'approvaltradogambar.tglbatas',
+                'approvaltradogambar.created_at',
+                'approvaltradogambar.updated_at',
+                'approvaltradogambar.modifiedby'
+            )
+            ->join(DB::raw("parameter with (readuncommitted)"), 'approvaltradogambar.statusapproval', 'parameter.id');
 
         $this->totalRows = $query->count();
         $this->totalPages = request()->limit > 0 ? ceil($this->totalRows / request()->limit) : 1;
@@ -43,21 +44,22 @@ class ApprovalTradoGambar extends MyModel
 
         return $data;
     }
-    
-    public function firstOrFind($trado_id){
+
+    public function firstOrFind($trado_id)
+    {
         $trado = Trado::find(request()->trado_id);
         $data = DB::table($this->table)
-        ->select(
-            'approvaltradogambar.id',
-            'approvaltradogambar.kodetrado',
-            'approvaltradogambar.statusapproval',
-            'approvaltradogambar.tglbatas',
-            'approvaltradogambar.created_at',
-            'approvaltradogambar.updated_at',
-            'approvaltradogambar.modifiedby'
-        )
-        ->where('kodetrado',$trado->kodetrado)->first();
-        
+            ->select(
+                'approvaltradogambar.id',
+                'approvaltradogambar.kodetrado',
+                'approvaltradogambar.statusapproval',
+                'approvaltradogambar.tglbatas',
+                'approvaltradogambar.created_at',
+                'approvaltradogambar.updated_at',
+                'approvaltradogambar.modifiedby'
+            )
+            ->where('kodetrado', $trado->kodetrado)->first();
+
 
         if (!$data) {
             $data = [
@@ -74,7 +76,7 @@ class ApprovalTradoGambar extends MyModel
         return $data;
     }
 
-    
+
     public function selectColumns($query)
     {
         return $query->from(
@@ -139,7 +141,7 @@ class ApprovalTradoGambar extends MyModel
                         if ($filters['field'] == 'statusapproval') {
                             $query = $query->where('parameter.text', '=', $filters['data']);
                         } else if ($filters['field'] == 'created_at' || $filters['field'] == 'updated_at') {
-                            $query = $query->whereRaw("format(".$this->table . "." . $filters['field'].", 'dd-MM-yyyy HH:mm:ss') LIKE '%$filters[data]%'");
+                            $query = $query->whereRaw("format(" . $this->table . "." . $filters['field'] . ", 'dd-MM-yyyy HH:mm:ss') LIKE '%$filters[data]%'");
                         } else {
                             $query = $query->where($this->table . '.' . $filters['field'], 'LIKE', "%$filters[data]%");
                         }
@@ -152,7 +154,7 @@ class ApprovalTradoGambar extends MyModel
                             if ($filters['field'] == 'statusapproval') {
                                 $query = $query->orWhere('parameter.text', '=', $filters['data']);
                             } else if ($filters['field'] == 'created_at' || $filters['field'] == 'updated_at') {
-                                $query = $query->orWhereRaw("format(".$this->table . "." . $filters['field'].", 'dd-MM-yyyy HH:mm:ss') LIKE '%$filters[data]%'");
+                                $query = $query->orWhereRaw("format(" . $this->table . "." . $filters['field'] . ", 'dd-MM-yyyy HH:mm:ss') LIKE '%$filters[data]%'");
                             } else {
                                 $query = $query->orWhere($this->table . '.' . $filters['field'], 'LIKE', "%$filters[data]%");
                             }
@@ -174,5 +176,134 @@ class ApprovalTradoGambar extends MyModel
     public function paginate($query)
     {
         return $query->skip($this->params['offset'])->take($this->params['limit']);
+    }
+    public function processStore(array $data): ApprovalTradoGambar
+    {
+        $approvalTradoGambar = new ApprovalTradoGambar();
+        $approvalTradoGambar->kodetrado = $data['kodetrado'];
+        $approvalTradoGambar->tglbatas = date('Y-m-d', strtotime($data['tglbatas']));
+        $approvalTradoGambar->statusapproval = $data['statusapproval'];
+        $approvalTradoGambar->modifiedby = auth('api')->user()->name;
+
+        $statusApproval = Parameter::from(DB::Raw("parameter with (readuncommitted)"))->select('id')->where('grp', '=', 'STATUS APPROVAL')->where('subgrp', '=', 'STATUS APPROVAL')->where('text', '=', 'APPROVAL')->first();
+        //nonaktif supir
+        if ($approvalTradoGambar->statusapproval == $statusApproval->id) {
+            $statusAktif = Parameter::from(DB::Raw("parameter with (readuncommitted)"))->select('id')->where('grp', '=', 'STATUS AKTIF')->where('subgrp', '=', 'STATUS AKTIF')->where('text', '=', 'AKTIF')->first();
+            $trado = Trado::where('kodetrado', $approvalTradoGambar->kodetrado)->first();
+            if ($trado) {
+                $trado->statusaktif = $statusAktif->id;
+                $trado->save();
+            }
+        }
+
+        if (!$approvalTradoGambar->save()) {
+            throw new \Exception('Error storing approvalTradoGambar.');
+        }
+
+        (new LogTrail())->processStore([
+            'namatabel' => $approvalTradoGambar->getTable(),
+            'postingdari' => 'ENTRY APPROVAL TRADO GAMBAR',
+            'idtrans' => $approvalTradoGambar->id,
+            'nobuktitrans' => $approvalTradoGambar->id,
+            'aksi' => 'ENTRY',
+            'datajson' => $approvalTradoGambar->toArray(),
+            'modifiedby' => auth('api')->user()->name
+        ]);
+
+        return $approvalTradoGambar;
+    }
+
+    public function processUpdate(ApprovalTradoGambar $approvaltradogambar, array $data): ApprovalTradoGambar
+    {
+        $approvaltradogambar->kodetrado = $data['kodetrado'];
+        $approvaltradogambar->tglbatas = date('Y-m-d', strtotime($data['tglbatas']));
+        $approvaltradogambar->statusapproval = $data['statusapproval'];
+        $approvaltradogambar->modifiedby = auth('api')->user()->name;
+
+        $statusApp = DB::table('parameter')->where('grp', 'STATUS APPROVAL')->where('subgrp', 'STATUS APPROVAL')->where('text', 'APPROVAL')->first();
+        $trado = DB::table('trado')->from(DB::raw("trado with (readuncommitted)"))
+            ->where('kodetrado', $data['kodetrado'])
+            ->first();
+        if ($trado != '') {
+            if ($data['statusapproval'] == $statusApp->id) {
+
+                $statusAktif = DB::table('parameter')->where('grp', 'STATUS AKTIF')->where('subgrp', 'STATUS AKTIF')->where('text', 'AKTIF')->first();
+                DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                    'statusaktif' => $statusAktif->id,
+                ]);
+            } else {
+
+                $statusNonAktif = DB::table('parameter')->where('grp', 'STATUS AKTIF')->where('subgrp', 'STATUS AKTIF')->where('text', 'NON AKTIF')->first();
+                if ($trado->photostnk == '' || $trado->phototrado == '' || $trado->photobpkb == '') {
+                    DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                        'statusaktif' => $statusNonAktif->id,
+                    ]);
+                    goto selesai;
+                } else {
+                    foreach (json_decode($trado->photobpkb) as $value) {
+                        if ($value != '') {
+                            if (!Storage::exists("trado/bpkb/$value")) {
+                                DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                                    'statusaktif' => $statusNonAktif->id,
+                                ]);
+                                goto selesai;
+                            }
+                        } else {
+                            DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                                'statusaktif' => $statusNonAktif->id,
+                            ]);
+                            goto selesai;
+                        }
+                    }
+                    foreach (json_decode($trado->photostnk) as $value) {
+                        if ($value != '') {
+                            if (!Storage::exists("trado/stnk/$value")) {
+                                DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                                    'statusaktif' => $statusNonAktif->id,
+                                ]);
+                                goto selesai;
+                            }
+                        } else {
+                            DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                                'statusaktif' => $statusNonAktif->id,
+                            ]);
+                            goto selesai;
+                        }
+                    }
+                    foreach (json_decode($trado->phototrado) as $value) {
+                        if ($value != '') {
+                            if (!Storage::exists("trado/trado/$value")) {
+                                DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                                    'statusaktif' => $statusNonAktif->id,
+                                ]);
+                                goto selesai;
+                            }
+                        } else {
+                            DB::table('trado')->where('kodetrado', $data['kodetrado'])->update([
+                                'statusaktif' => $statusNonAktif->id,
+                            ]);
+                            goto selesai;
+                        }
+                    }
+                }
+                selesai:
+            }
+        }
+
+        if (!$approvaltradogambar->save()) {
+            throw new \Exception('Error updating approvaltradogambar.');
+        }
+
+        (new LogTrail())->processStore([
+            'namatabel' => $approvaltradogambar->getTable(),
+            'postingdari' => 'EDIT APPROVAL TRADO GAMBAR',
+            'idtrans' => $approvaltradogambar->id,
+            'nobuktitrans' => $approvaltradogambar->id,
+            'aksi' => 'EDIT',
+            'datajson' => $approvaltradogambar->toArray(),
+            'modifiedby' => auth('api')->user()->name
+        ]);
+
+        return $approvaltradogambar;
     }
 }
