4eff077bfbaf685e06f2198ece03d618296ef0b2
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-01-30 16:15:53 +0700

    validasi buka cetak satu arah

---
+++ app/Rules/BukaCetakSatuArah.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Parameter;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Validation\Rule;
+
+class BukaCetakSatuArah implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $table = request()->table;
+        $allowed = false;
+        $statusBelumCetak = Parameter::where('grp', '=', 'STATUSCETAK')->where('text', '=', 'CETAK')->first();
+
+        foreach ($value as $val) {
+            $item = DB::table($table)->from(DB::raw("$table with (readuncommitted)"))->select('statuscetak')->where('id', $val)->where('statuscetak', $statusBelumCetak->id)->first();
+            if ($item) {
+                $allowed = true;
+            }
+        }
+        
+        return $allowed;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        $msg = 'PROSES TIDAK BISA LANJUT KARENA';
+        return "$msg<br> No Bukti belum dicetak ";
+    }
+}
