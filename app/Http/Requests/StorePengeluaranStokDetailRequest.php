d826d5c17dbb9cafc96519e58ea64baf19c25948
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-02-03 11:26:46 +0700

    kelompok pengeluaran stok harus ama

--- app/Http/Requests/StorePengeluaranStokDetailRequest.php
+++ app/Http/Requests/StorePengeluaranStokDetailRequest.php
@@ -118,4 +118,15 @@ class StorePengeluaranStokDetailRequest extends FormRequest
             'detail_persentasediscount.max' => ':attribute' . ' ' . app(ErrorController::class)->geterror('MIN')->keterangan,
         ];
     }
+    protected function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $kelompok = $this->input('detail_stok_kelompok');
+            dd($kelompok);
+            // Check if all values in kelompok are the same
+            if (count(array_unique($kelompok)) > 1) {
+                $validator->errors()->add('detail_stok_kelompok', 'Semua Stok harus dalam kelompok yang sama.');
+            }
+        });
+    }
 }
