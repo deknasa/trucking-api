e0e59cf935d4506e221d1cb80fef9d069de3423e
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-02-15 14:33:35 +0700

    Dto reminder Email

--- app/DataTransferObject/ReminderEmailDTO.php
+++ app/DataTransferObject/ReminderEmailDTO.php
@@ -7,20 +7,28 @@ class ReminderEmailDTO
 {
     public string $keterangan;
     public string $statusaktif;
+    public ?string $tas_id;
+    public ?string $accessTokenTnl;
 
     public function __construct(
         string $keterangan,
-        string $statusaktif
+        string $statusaktif,
+        ?string $tas_id,
+        ?string $accessTokenTnl
     ){
         $this->keterangan = $keterangan;
         $this->statusaktif = $statusaktif;
+        $this->tas_id = $tas_id;
+        $this->tas_id = $accessTokenTnl;
     }
 
 
     public static function dataRequest(StoreReminderEmailRequest $request){
         return new self(
             $request->input('keterangan'),
-            $request->input('statusaktif')
+            $request->input('statusaktif'),
+            $request->input('tas_id'),
+            $request->input('accessTokenTnl')
         );
     }
 }
