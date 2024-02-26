2aa92ec04f678a427567645e797fb42d6400855c
bshf <bungashafa53@gmail.com>
2024-02-26 11:06:26 +0700

    fix validasi upahsupir;

--- app/Rules/UniqueUpahSupirKotaSampaiEdit.php
+++ app/Rules/UniqueUpahSupirKotaSampaiEdit.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\ErrorController;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\DB;
 
-class UniqueUpahSupirKotaSampai implements Rule
+class UniqueUpahSupirKotaSampaiEdit implements Rule
 {
     /**
      * Create a new rule instance.
@@ -27,6 +27,8 @@ class UniqueUpahSupirKotaSampai implements Rule
      */
     public function passes($attribute, $value)
     {
+        $id = request()->id;
+        
         if (request()->penyesuaian == '') {
             $query = DB::table('upahsupir')
                 ->from(
@@ -38,7 +40,9 @@ class UniqueUpahSupirKotaSampai implements Rule
                 ->where('a.kotasampai_id', '=', (request()->kotasampai_id))
                 ->where('a.kotadari_id', '=', request()->kotadari_id)
                 ->whereRaw("isnull(a.penyesuaian,'')=''")
+                ->where('a.id', '<>', $id)
                 ->first();
+
             if (!isset($query)) {
 
                 $query = DB::table('upahsupir')
@@ -51,6 +55,7 @@ class UniqueUpahSupirKotaSampai implements Rule
                     ->where('a.kotadari_id', '=', (request()->kotasampai_id))
                     ->where('a.kotasampai_id', '=', request()->kotadari_id)
                     ->whereRaw("isnull(a.penyesuaian,'')=''")
+                    ->where('a.id', '<>', $id)
                     ->first();
             }
         } else {
@@ -64,6 +69,7 @@ class UniqueUpahSupirKotaSampai implements Rule
                 ->where('a.kotasampai_id', '=', (request()->kotasampai_id))
                 ->where('a.kotadari_id', '=', request()->kotadari_id)
                 ->where('a.penyesuaian', '=', request()->penyesuaian)
+                ->where('a.id', '<>', $id)
                 ->first();
 
             if (!isset($query)) {
@@ -77,6 +83,7 @@ class UniqueUpahSupirKotaSampai implements Rule
                     ->where('a.kotadari_id', '=', (request()->kotasampai_id))
                     ->where('a.kotasampai_id', '=', request()->kotadari_id)
                     ->where('a.penyesuaian', '=', request()->penyesuaian)
+                    ->where('a.id', '<>', $id)
                     ->first();
             }
         }
