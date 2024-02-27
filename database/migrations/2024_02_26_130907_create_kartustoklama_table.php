20f57313e9b0537b764b1b17b203ef5942dd3c26
Ryanita1402 <ryan_vixy1402@yahoo.com>
2024-02-27 15:57:28 +0700

    perbaikan seeder

--- database/migrations/2024_02_26_130907_create_kartustoklama_table.php
+++ database/migrations/2024_02_26_130907_create_kartustoklama_table.php
@@ -15,6 +15,23 @@ class CreateKartustoklamaTable extends Migration
     {
         Schema::create('kartustoklama', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('gudang_id')->nullable();
+            $table->unsignedBigInteger('trado_id')->nullable();
+            $table->unsignedBigInteger('gandengan_id')->nullable();
+            $table->unsignedBigInteger('stok_id')->nullable();
+            $table->string('lokasi', 1000)->nullable();
+            $table->string('kodebarang', 1000)->nullable();
+            $table->string('namabarang', 1000)->nullable();
+            $table->date('tglbukti')->nullable();
+            $table->string('nobukti', 1000)->nullable();
+            $table->unsignedBigInteger('kategori_id')->nullable();
+            $table->double('qtymasuk', 15, 2)->nullable();
+            $table->double('nilaimasuk', 15, 2)->nullable();
+            $table->double('qtykeluar', 15, 2)->nullable();
+            $table->double('nilaikeluar', 15, 2)->nullable();
+            $table->longText('info')->nullable();
+            $table->integer('urutfifo')->nullable();
+            $table->string('modifiedby', 50)->nullable();
             $table->timestamps();
         });
     }
