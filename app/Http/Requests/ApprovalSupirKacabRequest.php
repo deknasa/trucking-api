6948dc886d6cf4e4c7862b293d636abf10abeb50
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-04-25 13:22:34 +0700

    status approval supir harus sama untuk approval

--- app/Http/Requests/ApprovalSupirKacabRequest.php
+++ app/Http/Requests/ApprovalSupirKacabRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Error;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Http\Controllers\Api\ErrorController;
@@ -45,5 +46,27 @@ class ApprovalSupirKacabRequest extends FormRequest
         return [
             'Id.required' => 'Supir ' . app(ErrorController::class)->geterror('WP')->keterangan,
         ];
+    }
+
+    
+    protected function withValidator($validator)
+    {
+        $validator->after(function ($validator)  { 
+            $error = new Error();
+            $keteranganerror = $error->cekKeteranganError('ASB') ?? '';
+            // $keterangantambahanerror = $error->cekKeteranganError('PTBL') ?? '';
+            $id = $this->input('Id');
+            $statusapproval = [];
+            // $supirall = [];
+            foreach ($id as $index => $key) {
+                $supir = DB::table('supir')->where('id', $key)->first();
+                $statusapproval[] = $supir->statusapproval;
+                // $supirall[]=$supir;
+            }
+            if (count(array_unique($statusapproval)) > 1) {
+                $validator->errors()->add('Supir', $keteranganerror.'<br>'.$keterangantambahanerror);
+            }
+
+        });
     }
 }
