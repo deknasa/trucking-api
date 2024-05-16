22fa963ae941a77bf346f2a14500c7f5db9ebb24
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-05-16 11:43:54 +0700

    database name

--- config/database.php
+++ config/database.php
@@ -102,7 +102,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'web.transporindo.com'),
             'port' => env('DB_PORT', '1450'),
-            'database' => 'truckinglaraveltnl',
+            'database' => env('DB_DATABASE', 'trucking'),
             'username' => env('DB_USERNAME', 'sa'),
             'password' => $local,
             'charset' => 'utf8',
