60cc7c23a4935e1c78fd1732119304fda33f4fbe
Ryanita1402 <ryan_vixy1402@yahoo.com>
2024-05-17 09:37:18 +0700

    perbaikan front end

--- database/migrations/2022_09_27_000019_create_penerimaanstokheader_table.php
+++ database/migrations/2022_09_27_000019_create_penerimaanstokheader_table.php
@@ -57,6 +57,10 @@ class CreatePenerimaanstokheaderTable extends Migration
             $table->integer('statuskirimberkas')->Length(11)->nullable();
             $table->string('userkirimberkas',50)->nullable();
             $table->date('tglkirimberkas')->nullable();
+            $table->integer('statusapprovalpindahgudangspk')->Length(11)->nullable();
+            $table->string('userapprovalpindahgudangspk', 50)->nullable();
+            $table->date('tglapprovalpindahgudangspk')->nullable();
+            $table->dateTime('tglbataspindahgudangspk')->nullable();            
             $table->longText('info')->nullable();
             $table->string('modifiedby', 50)->nullable();
             $table->string('editing_by',50)->nullable();            
