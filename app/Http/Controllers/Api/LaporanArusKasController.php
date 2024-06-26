e3c70520f6337218f6862fb07b7f14211e3c7ccd
Ryanita1402 <ryan_vixy1402@yahoo.com>
2024-06-26 08:55:17 +0700

    perbaikan marketing

--- app/Http/Controllers/Api/LaporanArusKasController.php
+++ app/Http/Controllers/Api/LaporanArusKasController.php
@@ -33,162 +33,164 @@ class LaporanArusKasController extends Controller
         $periode = $request->periode;
         $laporanaruskas = new LaporanArusKas();
 
-        // $report = LaporanKasGantung::getReport($sampai, $jenis);
-        $saldoawal =
-            [
-                'keterangancoa' => 'SALDO AWAL',
-                'nominalawal' => 13787825468.08,
-                'nominalakhir' => 15727550277.84,
-            ];
-        $report = [
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK MASUK',
-                'type' => 'PENDAPATAN',
-                'keterangancoa' => 'CABANG JAKARTA',
-                'nominalawal' => 1690901500,
-                'nominalakhir' => 1449963000,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK MASUK',
-                'type' => 'PENDAPATAN',
-                'keterangancoa' => 'CABANG MAKASSAR',
-                'nominalawal' => 1532200522,
-                'nominalakhir' => 1557215705,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK MASUK',
-                'type' => 'PENDAPATAN',
-                'keterangancoa' => 'CABANG MANADO',
-                'nominalawal' => 179592500,
-                'nominalakhir' => 241842500,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK MASUK',
-                'type' => 'PENDAPATAN',
-                'keterangancoa' => 'CABANG MEDAN',
-                'nominalawal' => 859245750,
-                'nominalakhir' => 653224000,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK MASUK',
-                'type' => 'PENDAPATAN',
-                'keterangancoa' => 'CABANG SURABAYA',
-                'nominalawal' => 1168012800,
-                'nominalakhir' => 710078601,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK KELUAR',
-                'type' => 'BIAYA',
-                'keterangancoa' => 'B.Asuransi - Karyawan',
-                'nominalawal' => '-259662',
-                'nominalakhir' => '-259662',
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK KELUAR',
-                'type' => 'BIAYA',
-                'keterangancoa' => 'B.BANK - ADM',
-                'nominalawal' => '-5303244.19',
-                'nominalakhir' => '-4700475.23',
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK KELUAR',
-                'type' => 'BIAYA',
-                'keterangancoa' => 'B.DIREKSI',
-                'nominalawal' => '-3888300',
-                'nominalakhir' => '-3368200',
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK KELUAR',
-                'type' => 'BIAYA',
-                'keterangancoa' => 'B.GAJI DIREKSI',
-                'nominalawal' => '-120000000',
-                'nominalakhir' => '-120000000',
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK KELUAR',
-                'type' => 'BIAYA',
-                'keterangancoa' => 'B.GENSET',
-                'nominalawal' => '-12000',
-                'nominalakhir' => 0,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-            [
-                'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
-                'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
-                'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
-                'usercetak' => 'User :' . auth('api')->user()->name,
-                'jenisarus' => 'ARUS KAS/BANK KELUAR',
-                'type' => 'BIAYA',
-                'keterangancoa' => 'B.KANTOR - ADMINISTRASI',
-                'nominalawal' => '-5510204',
-                'nominalakhir' => 0,
-                'periodeawal' => 'Apr 2024',
-                'periodeakhir' => 'May 2024'
-            ],
-        ];
+        $hasil = $laporanaruskas->getReport($periode);
+        // $saldoawal =
+        //     [
+        //         'keterangancoa' => 'SALDO AWAL',
+        //         'nominalawal' => 13787825468.08,
+        //         'nominalakhir' => 15727550277.84,
+        //     ];
+        // $report = [
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK MASUK',
+        //         'type' => 'PENDAPATAN',
+        //         'keterangancoa' => 'CABANG JAKARTA',
+        //         'nominalawal' => 1690901500,
+        //         'nominalakhir' => 1449963000,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK MASUK',
+        //         'type' => 'PENDAPATAN',
+        //         'keterangancoa' => 'CABANG MAKASSAR',
+        //         'nominalawal' => 1532200522,
+        //         'nominalakhir' => 1557215705,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK MASUK',
+        //         'type' => 'PENDAPATAN',
+        //         'keterangancoa' => 'CABANG MANADO',
+        //         'nominalawal' => 179592500,
+        //         'nominalakhir' => 241842500,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK MASUK',
+        //         'type' => 'PENDAPATAN',
+        //         'keterangancoa' => 'CABANG MEDAN',
+        //         'nominalawal' => 859245750,
+        //         'nominalakhir' => 653224000,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK MASUK',
+        //         'type' => 'PENDAPATAN',
+        //         'keterangancoa' => 'CABANG SURABAYA',
+        //         'nominalawal' => 1168012800,
+        //         'nominalakhir' => 710078601,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK KELUAR',
+        //         'type' => 'BIAYA',
+        //         'keterangancoa' => 'B.Asuransi - Karyawan',
+        //         'nominalawal' => '-259662',
+        //         'nominalakhir' => '-259662',
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK KELUAR',
+        //         'type' => 'BIAYA',
+        //         'keterangancoa' => 'B.BANK - ADM',
+        //         'nominalawal' => '-5303244.19',
+        //         'nominalakhir' => '-4700475.23',
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK KELUAR',
+        //         'type' => 'BIAYA',
+        //         'keterangancoa' => 'B.DIREKSI',
+        //         'nominalawal' => '-3888300',
+        //         'nominalakhir' => '-3368200',
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK KELUAR',
+        //         'type' => 'BIAYA',
+        //         'keterangancoa' => 'B.GAJI DIREKSI',
+        //         'nominalawal' => '-120000000',
+        //         'nominalakhir' => '-120000000',
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK KELUAR',
+        //         'type' => 'BIAYA',
+        //         'keterangancoa' => 'B.GENSET',
+        //         'nominalawal' => '-12000',
+        //         'nominalakhir' => 0,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        //     [
+        //         'judulLaporan' => 'Laporan Arus Kas / Bank tahun 2024',
+        //         'judul' => 'PT. TRANSPORINDO AGUNG SEJAHTERA',
+        //         'tglcetak' => 'Tgl Cetak: ' . date('d-m-Y H:i:s'),
+        //         'usercetak' => 'User :' . auth('api')->user()->name,
+        //         'jenisarus' => 'ARUS KAS/BANK KELUAR',
+        //         'type' => 'BIAYA',
+        //         'keterangancoa' => 'B.KANTOR - ADMINISTRASI',
+        //         'nominalawal' => '-5510204',
+        //         'nominalakhir' => 0,
+        //         'periodeawal' => 'Apr 2024',
+        //         'periodeakhir' => 'May 2024'
+        //     ],
+        // ];
         return response([
             // 'data' => $laporankasgantung->getReport($periode)
-            'data' => $report,
-            'saldo' => $saldoawal,
+            // 'data' => $report,
+            // 'saldo' => $saldoawal,
+            'data' => $hasil['data'],
+            'saldo' => $hasil['dataSaldo'],
         ]);
     }
     /**
