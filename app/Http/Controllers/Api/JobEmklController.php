c0ee32be8d7ff380b23a12d864b9abaa10f46e40
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-08-20 10:34:50 +0700

    crud jobemkl

--- app/Http/Controllers/Api/JobEmklController.php
+++ app/Http/Controllers/Api/JobEmklController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Error;
 use App\Models\JobEmkl;
+use App\Models\MyModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RangeExportReportRequest;
+use Symfony\Component\HttpKernel\Controller\ErrorController;
 
 class JobEmklController extends Controller
 {
@@ -67,7 +71,7 @@ class JobEmklController extends Controller
             $data['tas_id'] = $jobEmkl->id;
 
             if ($cekStatusPostingTnl->text == 'POSTING TNL') {
-                $this->SaveTnlNew('jenistrado', 'add', $data);
+                $this->SaveTnlNew('jobemkl', 'add', $data);
             }
 
 
@@ -86,7 +90,11 @@ class JobEmklController extends Controller
 
     public function show($id)
     {
-        
+        $jobEmkl = new JobEmkl();
+        return response([
+            'status' => true,
+            'data' => $jobEmkl->findAll($id)
+        ]);
     }
     
     /**
@@ -95,16 +103,178 @@ class JobEmklController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $data = [
+                "tglbukti" => $request->tglbukti ?? '',
+                "shipper_id" => $request->shipper_id ?? '',
+                "shipper" => $request->shipper ?? '',
+                "tujuan_id" => $request->tujuan_id ?? '',
+                "tujuan" => $request->tujuan ?? '',
+                "container_id" => $request->container_id ?? '',
+                "container" => $request->container ?? '',
+                "jenisorder_id" => $request->jenisorder_id ?? '',
+                "jenisorder" => $request->jenisorder ?? '',
+                "kapal" => $request->kapal ?? '',
+                "destination" => $request->destination ?? '',
+                "nocont" => $request->nocont ?? '',
+                "noseal" => $request->noseal ?? '',
+                "accessTokenTnl" => $request->accessTokenTnl ?? '',
+
+            ];
+
+            $jobEmkl = new JobEmkl();
+            $jobEmkls = $jobEmkl->findOrFail($id);
+            $jobEmkl = $jobEmkl->processUpdate($jobEmkls, $data);
+            if ($request->from == '') {
+                $jobEmkl->position = $this->getPosition($jobEmkl, $jobEmkl->getTable())->position;
+                if ($request->limit == 0) {
+                    $jobEmkl->page = ceil($jobEmkl->position / (10));
+                } else {
+                    $jobEmkl->page = ceil($jobEmkl->position / ($request->limit ?? 10));
+                }
+            }
+
+
+            $cekStatusPostingTnl = DB::table("parameter")->from(DB::raw("parameter with (readuncommitted)"))->where('grp', 'STATUS POSTING TNL')->where('default', 'YA')->first();
+            $data['tas_id'] = $jobEmkl->id;
+
+            if ($cekStatusPostingTnl->text == 'POSTING TNL') {
+                $this->SaveTnlNew('jobemkl', 'edit', $data);
+            }
+            DB::commit();
+            return response([
+                'status' => true,
+                'message' => 'Berhasil disimpan',
+                'data' => $jobEmkl
+            ], 201);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    
     }
     
     /**
      * @ClassName 
      * @Keterangan HAPUS DATA
      */
-    public function destroy($id)
+    public function destroy(Request $request,$id)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $jobEmkl = new JobEmkl();
+            $jobEmkls = $jobEmkl->findOrFail($id);
+            $jobEmkl = $jobEmkl->processDestroy($jobEmkls);
+            if ($request->from == '') {
+                $selected = $this->getPosition($jobEmkl, $jobEmkl->getTable(), true);
+                $jobEmkl->position = $selected->position;
+                $jobEmkl->id = $selected->id;
+                if ($request->limit == 0) {
+                    $jobEmkl->page = ceil($jobEmkl->position / (10));
+                } else {
+                    $jobEmkl->page = ceil($jobEmkl->position / ($request->limit ?? 10));
+                }
+            }
+            
+
+            $cekStatusPostingTnl = DB::table("parameter")->from(DB::raw("parameter with (readuncommitted)"))->where('grp', 'STATUS POSTING TNL')->where('default', 'YA')->first();
+            $data['tas_id'] = $id;
+
+            $data["accessTokenTnl"] = $request->accessTokenTnl ?? '';
+
+            if ($cekStatusPostingTnl->text == 'POSTING TNL') {
+                // $this->SaveTnlNew('tujuan', 'delete', $data);
+            }
+            DB::commit();
+
+            return response([
+                'status' => true,
+                'message' => 'Berhasil dihapus',
+                'data' => $jobEmkl
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            throw $th;
+        }
+    }
+
+    public function cekValidasi($id)
+    {
+        $jobEmkl = new JobEmkl();
+        $dataMaster = $jobEmkl->where('id', $id)->first();
+        $error = new Error();
+        $keterangantambahanerror = $error->cekKeteranganError('PTBL') ?? '';
+        $user = auth('api')->user()->name;
+        $useredit = $dataMaster->editing_by ?? '';
+        $aksi = request()->aksi ?? '';
+
+        // $cekdata = $jobEmkl->cekvalidasihapus($id);
+        $cekdata = ["kondisi"=>false];
+        if ($cekdata['kondisi'] == true && $aksi != 'EDIT') {
+            $query = DB::table('error')
+                ->select(
+                    DB::raw("ltrim(rtrim(keterangan))+' (" . $cekdata['keterangan'] . ")' as keterangan")
+                )
+                ->where('kodeerror', '=', 'SATL')
+                ->get();
+            $keterangan = $query['0'];
+
+            $data = [
+                'status' => false,
+                'message' => $keterangan,
+                'errors' => '',
+                'kondisi' => $cekdata['kondisi'],
+            ];
+
+            return response($data);
+        } else  if ($useredit != '' && $useredit != $user) {
+            $waktu = (new Parameter())->cekBatasWaktuEdit('BATAS WAKTU EDIT MASTER');
+            $editingat = new DateTime(date('Y-m-d H:i:s', strtotime($dataMaster->editing_at)));
+            $diffNow = $editingat->diff(new DateTime(date('Y-m-d H:i:s')));
+            if ($diffNow->i > $waktu) {
+                if ($aksi != 'DELETE' && $aksi != 'EDIT') {
+                    (new MyModel())->updateEditingBy('jobemkl', $id, $aksi);
+                }
+
+                $data = [
+                    'status' => false,
+                    'message' => '',
+                    'errors' => '',
+                    'kondisi' => false,
+                    'editblok' => false,
+                ];
+
+                // return response($data);
+            } else {
+
+                $keteranganerror = $error->cekKeteranganError('SDE') ?? '';
+                $keterror = 'Data <b>' . $dataMaster->nobukti . '</b><br>' . $keteranganerror . ' <b>' . $useredit . '</b> <br> ' . $keterangantambahanerror;
+
+                $data = [
+                    'status' => true,
+                    'message' => ["keterangan" => $keterror],
+                    'errors' => '',
+                    'kondisi' => true,
+                    'editblok' => true,
+                ];
+
+                return response($data);
+            }
+        } else {
+            (new MyModel())->updateEditingBy('jobemkl', $id, $aksi);
+
+            $data = [
+                'status' => false,
+                'message' => '',
+                'errors' => '',
+                'kondisi' => $cekdata['kondisi'],
+            ];
+
+            return response($data);
+        }
     }
 
     public function default()
@@ -125,4 +295,106 @@ class JobEmklController extends Controller
             'data' => [],
         ]);
     }
+
+    /**
+     * @ClassName 
+     * @Keterangan EXPORT KE EXCEL
+     */
+    public function export(RangeExportReportRequest $request) {
+        if (request()->cekExport) {
+            
+            if (request()->offset == "-1" && request()->limit == '1') {
+                
+                return response([
+                    'errors' => [
+                        "export" => app(ErrorController::class)->geterror('DTA')->keterangan
+                    ],
+                    'status' => false,
+                    'message' => "The given data was invalid."
+                ], 422);
+            } else {
+                return response([
+                    'status' => true,
+                ]);
+            }
+        } else {
+            
+            $response = $this->index();
+            $decodedResponse = json_decode($response->content(), true);
+            $jobEmkl = $decodedResponse['data'];
+            
+            $judulLaporan = $jobEmkl[0]['judulLaporan'];
+            
+            $i = 0;
+            // foreach ($jobEmkl as $index => $params) {
+                
+            //     $statusaktif = $params['statusaktif'];
+                
+            //     $result = json_decode($statusaktif, true);
+                
+            //     $statusaktif = $result['MEMO'];
+                
+                
+            //     $jobEmkl[$i]['statusaktif'] = $statusaktif;
+                
+                
+            //     $i++;
+            // }
+            $columns = [
+                [
+                    'label' => 'No',
+                ],
+                [
+                    "label"=>"nobukti",
+                    "index"=>"nobukti",
+                ],
+                [
+                    "label"=>"tglbukti",
+                    "index"=>"tglbukti",
+                ],
+                [
+                    "label"=>"shipper",
+                    "index"=>"shipper",
+                ],
+                [
+                    "label"=>"tujuan",
+                    "index"=>"tujuan",
+                ],
+                [
+                    "label"=>"container",
+                    "index"=>"container",
+                ],
+                [
+                    "label"=>"jenis order",
+                    "index"=>"jenisorder",
+                ],
+                [
+                    "label"=>"kapal",
+                    "index"=>"kapal",
+                ],
+                [
+                    "label"=>"destination",
+                    "index"=>"destination",
+                ],
+                [
+                    "label"=>"nocont",
+                    "index"=>"nocont",
+                ],
+                [
+                    "label"=>"noseal",
+                    "index"=>"noseal",
+                ],
+            ];
+            
+            $this->toExcel($judulLaporan, $jobEmkl, $columns);
+        }
+    }
+    /**
+     * @ClassName 
+     * @Keterangan CETAK DATA
+     */
+    public function report()
+    {
+    }
+    
 }
