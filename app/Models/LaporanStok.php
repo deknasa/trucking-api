ad4c8732f6c4d0909157d5261bcd251aed255a93
Ryanita1402 <ryan_vixy1402@yahoo.com>
2024-10-02 15:36:12 +0700

    perbaikan backend

--- app/Models/LaporanStok.php
+++ app/Models/LaporanStok.php
@@ -299,8 +299,8 @@ class LaporanStok extends MyModel
                     'a.nobukti as nobukti',
                     db::raw("isnull(c1.kodekelompok,'')+' - '+trim(b.namastok) as id"),
                     db::raw("isnull(c1.kodekelompok,'')+' - '+trim(b.namastok) as kodebarang"),
-                    // db::raw("isnull(c1.kodekelompok,'')+' - '+trim(b.namastok) as namabarang"),
-                    db::raw("(case when isnull(b.keterangan,'')='' then a.namabarang else isnull(b.keterangan,'') end) as namabarang"),
+                    db::raw("isnull(c1.kodekelompok,'')+' - '+trim(b.namastok) as namabarang"),
+                    // db::raw("(case when isnull(b.keterangan,'')='' then a.namabarang else isnull(b.keterangan,'') end) as namabarang"),
                     'a.tglbukti as tglbukti',
                     db::raw("(isnull(a.qtymasuk,0)+
                         (case when a.nobukti='SALDO AWAL' then 
